7ae2a7e1c3bb58aa0424a079b30ee3a4d7368923
Iharob Al Asimi <iharob@gmail.com>
2021-04-12 15:49:37 -0400

    feat: Add accounts support (#19)

--- lib/src/solana_wallet.dart
+++ lib/src/solana_wallet.dart
@@ -1,25 +1,37 @@
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:cryptography/cryptography.dart';
 import 'package:solana/src/base58/base58.dart' as base58;
+import 'package:solana/src/types/hd_key_pair.dart';
 
 /// Signs transactions to be sent to a Solana Cluster
 class SolanaWallet {
-  SolanaWallet._fromKeyPair(KeyPair keyPair, this.address) : _keyPair = keyPair;
-
-  static final _ed25519 = Ed25519();
+  SolanaWallet._fromKeyPair(
+    this._keyPair, {
+    required this.address,
+  });
 
   /// Creates and initializes a new SolanaWallet for the given bip39
-  /// [mnemonic] string of 12 words
-  static Future<SolanaWallet> fromMnemonic(String mnemonic) async {
+  /// [mnemonic] string of 12 words.
+  ///
+  /// Multiple accounts can be attached to the same seed by using the [account]
+  /// parameter as index
+  static Future<SolanaWallet> fromMnemonic(
+    String mnemonic, {
+    int account = 0,
+  }) async {
     // Create the seed
     final List<int> seedBytes = bip39.mnemonicToSeed(mnemonic);
-    // Create a private key from the seed
-    final SecretKeyData privateKey = SecretKeyData(seedBytes.sublist(0, 32));
-    // Build the key pair
-    final keyPair = await _ed25519.newKeyPairFromSeed(privateKey.bytes);
-    final address = base58.encode((await keyPair.extractPublicKey()).bytes);
-    // Finally, create a new wallet
-    return SolanaWallet._fromKeyPair(keyPair, address);
+    final KeyPair keyPair = await _deriveKeyPair(seedBytes, account);
+    final PublicKey publicKey = await keyPair.extractPublicKey();
+    if (publicKey is SimplePublicKey) {
+      // Finally, create a new wallet
+      return SolanaWallet._fromKeyPair(
+        keyPair,
+        address: base58.encode(publicKey.bytes),
+      );
+    } else {
+      throw Exception('could not build a key pair');
+    }
   }
 
   /// Returns a Future that resolves to the result of signing
@@ -28,8 +40,23 @@ class SolanaWallet {
   Future<Signature> sign(List<int> data) =>
       _ed25519.sign(data, keyPair: _keyPair);
 
+  static final _ed25519 = Ed25519();
+  final KeyPair _keyPair;
+
   /// The address or public key of this wallet
   final String address;
 
-  final KeyPair _keyPair;
+  static String _getHDPath(int account) => "m/44'/501'/${account - 1}'";
+
+  static Future<KeyPair> _deriveKeyPair(
+    List<int> rawSeed,
+    int? account,
+  ) async {
+    final List<int> seed = rawSeed.sublist(0, 32);
+    if (account == null || account == 0) {
+      return _ed25519.newKeyPairFromSeed(seed);
+    } else {
+      return HDKeyPair.fromSeed(seed, _getHDPath(account));
+    }
+  }
 }
