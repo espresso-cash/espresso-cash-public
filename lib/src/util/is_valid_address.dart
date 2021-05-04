cee6df0babf2c2ffb60bb96f11d618695ed3c527
Iharob Al Asimi <iharob@gmail.com>
2021-05-04 19:01:58 -0400

    Document new exported method

--- lib/src/util/is_valid_address.dart
+++ lib/src/util/is_valid_address.dart
@@ -1,6 +1,8 @@
 import 'package:solana/src/base58/base58.dart';
 import 'package:solana/src/edwards25519/edwards_25519.dart';
 
+/// Returns true if [address] is a valid ed25519 point encoded
+/// to base58
 bool isValidAddress(String address) {
   try {
     final List<int> data = decode(address);
