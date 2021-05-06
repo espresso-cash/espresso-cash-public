02a609baf31f6abfd1646642e30f2e69d0265adc
Iharob Al Asimi <iharob@gmail.com>
2021-05-05 21:51:24 -0400

    Removed the curve stuff and used a library instead

--- lib/src/util/is_valid_address.dart
+++ lib/src/util/is_valid_address.dart
@@ -1,5 +1,5 @@
+import 'package:curve25519_kimble/curve25519_kimble.dart';
 import 'package:solana/src/base58/base58.dart';
-import 'package:solana/src/edwards25519/edwards_25519.dart';
 
 /// Returns true if [address] is a valid ed25519 point encoded
 /// to base58
