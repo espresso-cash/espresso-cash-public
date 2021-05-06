f0bcf9a2d5c9e3dc3fbcd88a3c3c290be7bcc816
Iharob Al Asimi <iharob@gmail.com>
2021-05-05 21:58:16 -0400

    Used padLeft directly despite making it unreadable: it is just they way cool kids do it

--- lib/src/util/solana_int_encoder.dart
+++ lib/src/util/solana_int_encoder.dart
@@ -3,15 +3,11 @@ import 'package:convert/convert.dart';
 const int _bitsPerByte = 8;
 const int _hexCharsPerByte = 2;
 
-String _padTo(String what, String character, int length) =>
-    what.padLeft(length, character);
-
 extension SolanaEncoder on int {
   List<int> toSolanaBytes([int bitSize = 8]) {
-    final String padded = _padTo(
-      toRadixString(16),
-      '0',
+    final String padded = toRadixString(16).padLeft(
       _hexCharsPerByte * bitSize ~/ _bitsPerByte,
+      '0',
     );
     final List<int> be = hex.decode(padded);
     // Convert to LE
