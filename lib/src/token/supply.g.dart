a8fbb1d2c945f830f569fc6ebeb8121a85540ab4
Iharob Al Asimi <iharob@gmail.com>
2021-06-16 21:05:35 -0400

    Add `getTokenSupply` RPC method and a test.
    - Note that the token address that is being used for the test needs to be specified in a more robust way for this to pass the testsuite.

---
+++ lib/src/token/supply.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'supply.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TokenSupply _$TokenSupplyFromJson(Map<String, dynamic> json) {
+  return TokenSupply(
+    amount: json['amount'] as String,
+    decimals: json['decimals'] as int,
+  );
+}
+
+TokenSupplyResult _$TokenSupplyResultFromJson(Map<String, dynamic> json) {
+  return TokenSupplyResult(
+    context: json['context'],
+    value: TokenSupply.fromJson(json['value'] as Map<String, dynamic>),
+  );
+}
+
+GetTokenSupplyResponse _$GetTokenSupplyResponseFromJson(
+    Map<String, dynamic> json) {
+  return GetTokenSupplyResponse(
+    TokenSupplyResult.fromJson(json['result'] as Map<String, dynamic>),
+  );
+}
