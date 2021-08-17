5a77862302bd7f222f30d8efd1fe42c644b4c209
Iharob Al Asimi <iharob@gmail.com>
2021-08-17 02:46:48 -0400

    Export associated token account. The AssociatedTokenAccount object.

--- packages/solana/lib/solana.dart
+++ packages/solana/lib/solana.dart
@@ -4,6 +4,7 @@ export 'src/encoder/encoder.dart'
     show Instruction, Message, Buffer, AccountMeta;
 export 'src/memo_program/memo_program.dart';
 export 'src/rpc_client/rpc_client.dart';
+export 'src/spl_token/associated_account.dart';
 export 'src/spl_token/spl_token.dart';
 export 'src/system_program/system_program.dart';
 export 'src/token_program/token_program.dart';
