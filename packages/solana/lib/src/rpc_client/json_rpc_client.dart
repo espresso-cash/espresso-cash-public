8bfc845a8fe8e3d360a35e2d13b2a914c99d89a3
Arturo Jamaica <me@arturojamaica.com>
2021-09-08 20:03:58 +0900

    Map response to list

--- packages/solana/lib/src/rpc_client/json_rpc_client.dart
+++ packages/solana/lib/src/rpc_client/json_rpc_client.dart
@@ -48,7 +48,7 @@ class JsonRpcClient {
       return data;
     }
   }
-  Future<Map<String, dynamic>> multiRequest(List<Map<String, dynamic>> requests) async {
+  Future<List<dynamic>> multiRequest(List<Map<String, dynamic>> requests) async {
     // Perform the POST requests
     final http.Response response = await http.post(
       Uri.parse(_url),
@@ -61,17 +61,7 @@ class JsonRpcClient {
     if (response.statusCode != 200) {
       throw HttpException(response.statusCode, response.body);
     } else {
-      final data = json.decode(response.body) as Map<String, dynamic>;
-      if (data['jsonrpc'] != '2.0') {
-        throw const FormatException('invalid jsonrpc-2.0 response');
-      }
-      if (data['error'] != null) {
-        throw JsonRpcException.fromJson(data['error'] as Map<String, dynamic>);
-      }
-      if (!data.containsKey('result')) {
-        throw const FormatException(
-            'object has no result field, invalid jsonrpc-2.0');
-      }
+      final data = json.decode(response.body) as List<dynamic>;
       return data;
     }
   }
