9e834e9c109085f51865a0b450a10f807e25590a
Iharob Al Asimi <iharob@gmail.com>
2021-10-26 09:06:32 -0400

    Add some missing generated files

---
+++ packages/solana/lib/src/dto/mint_account_data_info.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mint_account_data_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MintAccountDataInfo _$MintAccountDataInfoFromJson(Map<String, dynamic> json) =>
+    MintAccountDataInfo(
+      mintAuthority: json['mintAuthority'] as String?,
+      freezedAuthority: json['freezedAuthority'] as String?,
+      isInitialized: json['isInitialized'] as bool,
+      decimals: json['decimals'] as int,
+      supply: json['supply'] as String,
+    );
+
+Map<String, dynamic> _$MintAccountDataInfoToJson(
+        MintAccountDataInfo instance) =>
+    <String, dynamic>{
+      'mintAuthority': instance.mintAuthority,
+      'freezedAuthority': instance.freezedAuthority,
+      'isInitialized': instance.isInitialized,
+      'supply': instance.supply,
+      'decimals': instance.decimals,
+    };
---
+++ packages/solana/lib/src/dto/parsed_account_data.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'parsed_account_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SplTokenProgramAccountData _$$SplTokenProgramAccountDataFromJson(
+        Map<String, dynamic> json) =>
+    _$SplTokenProgramAccountData(
+      ParsedSplTokenAccountData.fromJson(
+          json['parsed'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$SplTokenProgramAccountDataToJson(
+        _$SplTokenProgramAccountData instance) =>
+    <String, dynamic>{
+      'parsed': instance.parsed,
+    };
+
+_$StakeProgramAccountData _$$StakeProgramAccountDataFromJson(
+        Map<String, dynamic> json) =>
+    _$StakeProgramAccountData(
+      ParsedStakeAccountData.fromJson(json['parsed'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$StakeProgramAccountDataToJson(
+        _$StakeProgramAccountData instance) =>
+    <String, dynamic>{
+      'parsed': instance.parsed,
+    };
+
+_$UnsupportedProgramAccountData _$$UnsupportedProgramAccountDataFromJson(
+        Map<String, dynamic> json) =>
+    _$UnsupportedProgramAccountData(
+      json['parsed'] as Map<String, dynamic>,
+    );
+
+Map<String, dynamic> _$$UnsupportedProgramAccountDataToJson(
+        _$UnsupportedProgramAccountData instance) =>
+    <String, dynamic>{
+      'parsed': instance.parsed,
+    };
