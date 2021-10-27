c6153d23fce388082c2354d01b19579c3e386dfe
Iharob Al Asimi <iharob@gmail.com>
2021-10-27 14:58:08 -0400

    Resotre lock file
    To make `NotificationParams.result` generic again

--- packages/solana/lib/src/subscription_client/notification_message.dart
+++ packages/solana/lib/src/subscription_client/notification_message.dart
@@ -1,6 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:solana/src/dto/account.dart';
+import 'package:solana/src/dto/logs.dart';
+import 'package:solana/src/dto/slot.dart';
 import 'package:solana/src/subscription_client/abstract_message.dart';
 import 'package:solana/src/subscription_client/notification_params.dart';
+import 'package:solana/src/subscription_client/optional_error.dart';
 
 part 'notification_message.freezed.dart';
 part 'notification_message.g.dart';
@@ -14,23 +18,23 @@ class NotificationMessage
   const factory NotificationMessage.unsupported() = _UnsupportedNotification;
 
   const factory NotificationMessage.accountNotification({
-    required NotificationParamsAccount params,
+    required NotificationParams<Account> params,
   }) = AccountNotification;
 
   const factory NotificationMessage.logsNotification({
-    required NotificationParamsLogs params,
+    required NotificationParams<Logs> params,
   }) = LogsNotification;
 
   const factory NotificationMessage.programNotification({
-    required NotificationParamsGeneric params,
+    required NotificationParams<dynamic> params,
   }) = ProgramNotification;
 
   const factory NotificationMessage.signatureNotification({
-    required NotificationParamsSignature params,
+    required NotificationParams<OptionalError> params,
   }) = SignatureNotification;
 
   const factory NotificationMessage.slotNotification({
-    required NotificationParamsSlot params,
+    required NotificationParams<Slot> params,
   }) = SlotNotification;
 
   factory NotificationMessage.fromJson(Map<String, dynamic> json) =>
