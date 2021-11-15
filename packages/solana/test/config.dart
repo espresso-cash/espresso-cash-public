fa11e4b13c5e9fe077e6cbd2861339d442201fa1
ookami-kb <ookami.kb@gmail.com>
2021-11-15 02:10:08 +0100

    WIP

--- packages/solana/test/config.dart
+++ packages/solana/test/config.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-final devnetRpcUrl = Platform.environment['DEVNET_RPC_URL'] ??
-    'http://cryptoplease.devnet.rpcpool.com';
+final devnetRpcUrl =
+    Platform.environment['DEVNET_RPC_URL'] ?? 'http://127.0.0.1:8899';
 final devnetWebsocketUrl =
     Platform.environment['DEVNET_WEBSOCKET_URL'] ?? 'ws://127.0.0.1:8900';
