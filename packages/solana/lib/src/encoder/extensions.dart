fa6b71067fd05dfc3c3c459fce688fbabf2c15e0
Antoine Herzog <antoine@iov.one>
2021-12-17 11:44:35 +0100

    unused

--- packages/solana/lib/src/encoder/extensions.dart
+++ packages/solana/lib/src/encoder/extensions.dart
@@ -1,4 +1,3 @@
-import 'package:solana/src/crypto/ed25519_hd_keypair.dart';
 import 'package:solana/src/encoder/account_meta.dart';
 import 'package:solana/src/encoder/buffer.dart';
 import 'package:solana/src/encoder/instruction.dart';
