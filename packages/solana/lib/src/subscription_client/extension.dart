efe2da7083b0d3706cd87e35fca079a7f72560c1
Iharob Al Asimi <iharob@gmail.com>
2021-12-22 10:17:13 -0400

    Throw an exception if the signature subscribe receives an error

--- packages/solana/lib/src/subscription_client/extension.dart
+++ packages/solana/lib/src/subscription_client/extension.dart
@@ -4,7 +4,12 @@ extension SubscriptionClientConvenienceMethods on SubscriptionClient {
   Future<void> waitForSignatureStatus(
     String signature, {
     required ConfirmationStatus status,
-  }) {
-    return signatureSubscribe(signature, commitment: status).first;
+  }) async {
+    final result =
+        await signatureSubscribe(signature, commitment: status).first;
+    if (result.err != null) {
+      // FIXME(IA): use the error information
+      throw Exception('an error occurred while waiting for a signature status');
+    }
   }
 }
