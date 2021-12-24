21fc7471bf6d11947ffd60812db8e4b451a3d301
ookami-kb <ookami.kb@gmail.com>
2021-12-24 20:42:39 +0100

    revert config

--- packages/solana/test/config.dart
+++ packages/solana/test/config.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
 final devnetRpcUrl =
-    Platform.environment['DEVNET_RPC_URL'] ?? 'http://api.devnet.solana.com';
-final devnetWebsocketUrl = Platform.environment['DEVNET_WEBSOCKET_URL'] ??
-    'ws://api.devnet.solana.com';
+    Platform.environment['DEVNET_RPC_URL'] ?? 'http://127.0.0.1:8899';
+final devnetWebsocketUrl =
+    Platform.environment['DEVNET_WEBSOCKET_URL'] ?? 'ws://127.0.0.1:8900';
