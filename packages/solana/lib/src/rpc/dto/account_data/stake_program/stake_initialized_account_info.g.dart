5f2d2c9d766954f67144f1a54740b029a1fe548e
Iharob Al Asimi <85121160+iharobCP@users.noreply.github.com>
2022-01-10 12:56:22 -0400

    fix: Add initialized account to StakeAccountData union (#151)

---
+++ packages/solana/lib/src/rpc/dto/account_data/stake_program/stake_initialized_account_info.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stake_initialized_account_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+StakeInitializedAccountInfo _$StakeInitializedAccountInfoFromJson(
+        Map<String, dynamic> json) =>
+    StakeInitializedAccountInfo(
+      meta: StakeMeta.fromJson(json['meta'] as Map<String, dynamic>),
+      stake: Stake.fromJson(json['stake'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$StakeInitializedAccountInfoToJson(
+        StakeInitializedAccountInfo instance) =>
+    <String, dynamic>{
+      'meta': instance.meta,
+      'stake': instance.stake,
+    };
