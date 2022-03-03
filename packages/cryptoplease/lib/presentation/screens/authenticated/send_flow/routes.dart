cebc8edcb091a451cf166a9c1eab071f43322312
Ricardo Brunetto <ricardohb18@gmail.com>
2022-03-03 08:06:47 -0300

    fix linter issues

--- packages/cryptoplease/lib/presentation/screens/authenticated/send_flow/routes.dart
+++ packages/cryptoplease/lib/presentation/screens/authenticated/send_flow/routes.dart
@@ -3,9 +3,9 @@ import 'package:cryptoplease/bl/qr_scanner/qr_scanner_request.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/confirm_screen/confirm_screen.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/enter_address/enter_address_screen.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/enter_amount/enter_amount_screen.dart';
-import 'package:cryptoplease/presentation/screens/authenticated/send_flow/send_nft_flow.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/pick_recipient_type/pick_recipient_type_screen.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/qr_scanner_screen/qr_scanner_screen.dart';
+import 'package:cryptoplease/presentation/screens/authenticated/send_flow/send_nft_flow.dart';
 import 'package:cryptoplease/presentation/screens/authenticated/send_flow/send_token_flow.dart';
 
 const createPaymentFlowRoutes = [
