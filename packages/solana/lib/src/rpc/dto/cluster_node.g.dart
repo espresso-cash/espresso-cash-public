9dd5c9d5edfbfd84b5aad1b8a690247d471a520e
Kirill Bubochkin <ookami.kb@gmail.com>
2022-03-05 20:19:55 +0100

    fix: Make ClusterNode fields nullable (#192)

--- packages/solana/lib/src/rpc/dto/cluster_node.g.dart
+++ packages/solana/lib/src/rpc/dto/cluster_node.g.dart
@@ -8,8 +8,8 @@ part of 'cluster_node.dart';
 
 ClusterNode _$ClusterNodeFromJson(Map<String, dynamic> json) => ClusterNode(
       pubkey: json['pubkey'] as String,
-      gossip: json['gossip'] as String,
-      tpu: json['tpu'] as String,
+      gossip: json['gossip'] as String?,
+      tpu: json['tpu'] as String?,
       rpc: json['rpc'] as String?,
       version: json['version'] as String?,
       featureSet: json['featureSet'] as int?,
