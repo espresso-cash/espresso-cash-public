bd6606659c9c67c1643c8e1ea0af147a3728ed01
ookami-kb <ookami.kb@gmail.com>
2022-03-22 22:40:33 +0100

    upd

--- packages/cryptoplease/lib/data/offchain_metadata_repository.dart
+++ packages/cryptoplease/lib/data/offchain_metadata_repository.dart
@@ -3,22 +3,11 @@ import 'package:solana/metaplex.dart';
 
 class OffchainMetadataRepositoryImpl implements OffchainMetadataRepository {
   final Map<String, Future<OffChainMetadata>> _requests = {};
-  final Map<String, OffChainMetadata> _data = {};
 
   @override
-  Future<OffChainMetadata> getMetadata(Metadata metadata) async {
-    final saved = _data[metadata.mint];
-    if (saved != null) {
-      return saved;
-    }
-
-    return _requests.putIfAbsent(
-      metadata.mint,
-      () => metadata.getExternalJson().then((data) {
-        _data[metadata.mint] = data;
-
-        return data;
-      }),
-    );
-  }
+  Future<OffChainMetadata> getMetadata(Metadata metadata) async =>
+      _requests.putIfAbsent(
+        metadata.mint,
+        () => metadata.getExternalJson(),
+      );
 }
