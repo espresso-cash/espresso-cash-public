153e0b4bf03ad82170c0d4eee82712b791aa3ac0
ookami-kb <ookami.kb@gmail.com>
2022-04-11 00:26:29 +0200

    fix: Enable receive flow for prod

--- packages/cryptoplease/lib/presentation/screens/authenticated/receive_flow/flow.dart
+++ packages/cryptoplease/lib/presentation/screens/authenticated/receive_flow/flow.dart
@@ -1,20 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cryptoplease/bl/tokens/token.dart';
-import 'package:cryptoplease/config.dart';
 import 'package:cryptoplease/presentation/routes.dart';
-import 'package:cryptoplease_ui/cryptoplease_ui.dart';
 import 'package:flutter/material.dart';
 
 extension ReceiveFlowExt on BuildContext {
   void navigateToReceiveFlow({Token? token}) => navigateTo(
         RequestTypeRoute(
-          onLinkSelected: () {
-            if (isProd) {
-              showCpSnackbar(this, message: 'Coming soon!');
-            } else {
-              navigateTo(LinkRequestFlowRoute(initialToken: token));
-            }
-          },
+          onLinkSelected: () =>
+              navigateTo(LinkRequestFlowRoute(initialToken: token)),
           onAddressSelected: () => navigateTo(const ShareAddressRoute()),
         ),
       );
