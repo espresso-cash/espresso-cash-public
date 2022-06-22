73842a2ade0dc734493cf97fbaef9dff7923b4ac
Ricardo Brunetto <ricardo.brunetto@eurekalabs.com.br>
2022-06-22 08:49:58 -0300

    signing and sending transaction with Phantom

--- packages/cryptoplease_link/public/scripts/phantom-app.js
+++ packages/cryptoplease_link/public/scripts/phantom-app.js
@@ -2,56 +2,56 @@ const checkPhantom = function () {
   const isPhantomInstalled = window.solana && window.solana.isPhantom;
 
   if (isPhantomInstalled) {
-    document.getElementById("phantom-button").style.visibility = "visible";
+    document.getElementById('phantom-button').style.visibility = 'visible';
   } else {
-    document.getElementById("phantom-button").style.visibility = "hidden";
+    document.getElementById('phantom-button').style.visibility = 'hidden';
   }
 };
 
-const connect = function () {
-  window.solana.connect().then(_sendTransaction).catch(_onError);
+const sendTransaction = function (amount, recipient) {
+  window.solana
+    .connect()
+    .then(({ publicKey }) => _createAndSendTransaction(publicKey, amount, recipient))
+    .catch(_onError);
 };
 
-const _onError = console.log;
+const _onError = console.warn;
 
-const _createTransferTransaction = async (connection, publicKey, lamports) => {
+const _getLamports = function (amount) {
+  const multiplier = 10 ** 9;
+  return amount * multiplier;
+};
+
+const _createTransfer = async (connection, publicKey, destination, lamports) => {
   if (!publicKey) return;
   let transaction = new solanaWeb3.Transaction().add(
     solanaWeb3.SystemProgram.transfer({
       fromPubkey: publicKey,
-      // TODO: change it
-      toPubkey: publicKey,
+      toPubkey: destination,
       lamports: lamports,
-    })
+    }),
   );
   transaction.feePayer = publicKey;
-  console.log("Getting recent blockhash");
   const anyTransaction = transaction;
-  anyTransaction.recentBlockhash = (
-    await connection.getRecentBlockhash()
-  ).blockhash;
+  anyTransaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
   return transaction;
 };
 
-const _sendTransaction = async function (data) {
-    // TODO: get from env
-    const { publicKey } = data;
-    const network = "https://api.devnet.solana.com";
-    const connection = new solanaWeb3.Connection(network);
-    try {
-      // TODO: change it
-      const transaction = await _createTransferTransaction(connection, publicKey, 100);
-      if (!transaction) return;
-      const provider = window.solana;
-      let signed = await provider.signTransaction(transaction);
-      console.log("Got signature, submitting transaction");
-      let signature = await connection.sendRawTransaction(signed.serialize());
-      console.log("Submitted transaction " + signature + ", awaiting confirmation");
-      await connection.confirmTransaction(signature);
-      console.log("Transaction " + signature + " confirmed");
-    } catch (err) {
-      console.warn(err);
-      console.log("[error] sendTransaction: " + JSON.stringify(err));
-    }
-  };
-  
+const _createAndSendTransaction = async function (publicKey, amount, destination) {
+  const network = 'https://api.mainnet-beta.solana.com';
+  const connection = new solanaWeb3.Connection(network);
+
+  try {
+    const lamports = _getLamports(amount);
+    const transaction = await _createTransfer(connection, publicKey, destination, lamports);
+    if (!transaction) return;
+    const provider = window.solana;
+    const { signature } = await provider.signAndSendTransaction(transaction);
+    await connection.confirmTransaction(signature);
+
+    alert('Transaction confirmed!');
+  } catch (err) {
+    console.warn(err);
+    alert('Error sending the transaction!');
+  }
+};
