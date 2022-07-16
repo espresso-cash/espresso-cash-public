24f67df92a0e2fadcaed4f5ec2d877dcdcddbc08
Ricardo Brunetto <ricardo.brunetto@eurekalabs.com.br>
2022-07-16 14:18:33 -0300

    fix: layout for solana shareable (#316)

--- public/script.js
+++ public/script.js
@@ -1,5 +1,13 @@
-document.querySelectorAll('.js-copy').forEach(element => {
-    element.addEventListener('click', function (e) {
-        navigator.clipboard.writeText('{{ deepLink }}');
-    });
-});
+const openDeepLink = function (shouldCopy, deepLink) {
+    if (shouldCopy) {
+        CopyToClipboard(deepLink)
+    }
+    window.location = deepLink;
+}
+
+const openInstallLink = function (shouldCopy, deepLink, installLink) {
+    if (shouldCopy) {
+        CopyToClipboard(deepLink)
+    }
+    window.location = installLink;
+}
