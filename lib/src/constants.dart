b22f193fcfba628c12bb66cd8fd85875067f2d5a
Ricardo Brunetto <ricardo.brunetto@eurekalabs.com.br>
2022-07-17 16:33:19 -0300

    feat: mapping universal link to deeplink (#323)

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -2,3 +2,8 @@ const cpLinkDomain = 'cryptoplease.link';
 const link1Host = 'solana1.$cpLinkDomain';
 const link2Host = 'solana2.$cpLinkDomain';
 const solanaPayHost = 'solanapay.$cpLinkDomain';
+const deeplinkScheme = 'cryptoplease-sol';
+const protocolMap = {
+  link1Host: '1',
+  link2Host: '2',
+};
