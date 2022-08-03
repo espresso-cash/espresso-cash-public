a331d545950996032ab4686f842bc0ff3a8acc61
Ricardo Brunetto <ricardohb18@gmail.com>
2022-08-03 08:19:56 -0300

    minor fixes and ui tests

--- packages/cryptoplease/lib/features/request_pay/presentation/components/input_display.dart
+++ packages/cryptoplease/lib/features/request_pay/presentation/components/input_display.dart
@@ -18,14 +18,16 @@ class InputDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final locale = DeviceLocale.localeOf(context);
+    final decimalSeparator = getDecimalSeparator(locale);
     const defaultValue = Amount.crypto(value: 0, currency: Currency.sol);
     final defaultMask = defaultValue.format(locale, skipSymbol: true);
-    final mask = input.differenceMask(locale, defaultMask);
+    final sanitizedInput = input.sanitize(decimalSeparator);
+    final mask = sanitizedInput.differenceMask(decimalSeparator, defaultMask);
 
     return FittedBox(
       child: Text.rich(
         TextSpan(
-          text: input,
+          text: sanitizedInput,
           children: [
             TextSpan(
               text: mask,
@@ -46,14 +48,16 @@ class InputDisplay extends StatelessWidget {
 }
 
 extension on String {
-  String differenceMask(Locale locale, String defaultMask) {
-    var inputValue = this;
-    final decimalSeparator = getDecimalSeparator(locale);
+  String sanitize(String decimalSeparator) {
+    if (isNotEmpty && this[0] == decimalSeparator) return '0$decimalSeparator';
+
+    return this;
+  }
+
+  String differenceMask(String decimalSeparator, String defaultMask) {
+    final inputValue = this;
 
     if (inputValue.isEmpty) return defaultMask;
-    if (inputValue[0] == decimalSeparator) {
-      inputValue = '0$inputValue';
-    }
 
     final values = inputValue.split(decimalSeparator);
     final hasDecimal = values.length > 1;
