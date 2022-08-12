70059cc7cfa2cc76dca130e9ab2397209a6bd8e6
ookami-kb <ookami.kb@gmail.com>
2022-08-12 21:52:33 +0200

    upd

--- packages/jsonrpc_client/lib/builder.dart
+++ packages/jsonrpc_client/lib/builder.dart
@@ -2,5 +2,5 @@ import 'package:build/build.dart';
 import 'package:jsonrpc_client/src/client_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder clientBuilder(BuilderOptions options) =>
+Builder clientBuilder(BuilderOptions _) =>
     PartBuilder([ClientGenerator()], '.rpc.dart');
