73baf211789f53bb0966190b9d6ac39652a184aa
Justin Enerio <justinenerio@pm.me>
2022-09-22 10:04:13 +0800

    removed unused button

--- packages/cryptoplease/lib/app/screens/authenticated/components/header_buttons.dart
+++ packages/cryptoplease/lib/app/screens/authenticated/components/header_buttons.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cryptoplease/app/routes.dart';
 import 'package:cryptoplease/app/screens/authenticated/receive_flow/flow.dart';
-import 'package:cryptoplease/core/accounts/bl/account.dart';
 import 'package:cryptoplease/core/tokens/token.dart';
 import 'package:cryptoplease/features/outgoing_transfer/presentation/send_flow/fungible_token/send_flow.dart';
 import 'package:cryptoplease/gen/assets.gen.dart';
@@ -9,24 +8,6 @@ import 'package:cryptoplease/l10n/l10n.dart';
 import 'package:cryptoplease_ui/cryptoplease_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
-
-// TODO(JE): remove if not used in other pages
-class AddFundsButton extends StatelessWidget {
-  const AddFundsButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => HeaderedListButton(
-        label: context.l10n.addFunds,
-        icon: SvgPicture.asset(Assets.icons.add.path),
-        onPressed: () => context.router.navigate(
-          AddFundsRoute(
-            wallet: context.read<MyAccount>().wallet,
-            token: Token.sol,
-          ),
-        ),
-      );
-}
 
 class SwapButton extends StatelessWidget {
   const SwapButton({Key? key, this.token}) : super(key: key);
