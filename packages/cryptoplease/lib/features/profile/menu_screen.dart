535d34b43e01e0d68c3d5e549dd3cdf53b5efdb5
Justin Enerio <justinenerio@pm.me>
2022-09-24 18:22:24 +0800

    feat: add pull-to-refresh in balance page (#446)

--- packages/cryptoplease/lib/features/profile/menu_screen.dart
+++ packages/cryptoplease/lib/features/profile/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:cryptoplease/app/components/refresh_balance_wrapper.dart';
 import 'package:cryptoplease/features/profile/components/menu_header.dart';
 import 'package:cryptoplease/features/profile/components/menu_section.dart';
 import 'package:cryptoplease_ui/cryptoplease_ui.dart';
+import 'package:dfunc/dfunc.dart';
 import 'package:flutter/material.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -9,19 +10,33 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => RefreshBalancesWrapper(
-        builder: (context, _) => CpTheme.light(
-          child: ColoredBox(
-            color: Colors.white,
-            child: SafeArea(
-              bottom: false,
-              child: ColoredBox(
-                color: CpColors.lightGreyBackground,
-                child: ListView(
-                  physics: const ClampingScrollPhysics(),
-                  children: const [
-                    MenuHeader(),
-                    MenuSection(),
-                  ],
+        builder: (context, onRefresh) => CpTheme.light(
+          child: RefreshIndicator(
+            displacement: 80,
+            notificationPredicate: T,
+            onRefresh: onRefresh,
+            backgroundColor: Colors.white,
+            color: CpColors.primaryColor,
+            child: DecoratedBox(
+              decoration: _menuScreenBackground,
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.white,
+                  child: SafeArea(
+                    bottom: false,
+                    child: ColoredBox(
+                      color: CpColors.lightGreyBackground,
+                      child: ListView(
+                        physics: const ClampingScrollPhysics(),
+                        children: const [
+                          MenuHeader(),
+                          MenuSection(),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -29,3 +44,15 @@ class MenuScreen extends StatelessWidget {
         ),
       );
 }
+
+const _menuScreenBackground = BoxDecoration(
+  gradient: LinearGradient(
+    stops: [0.5, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: <Color>[
+      Colors.white,
+      CpColors.lightGreyBackground,
+    ],
+  ),
+);
