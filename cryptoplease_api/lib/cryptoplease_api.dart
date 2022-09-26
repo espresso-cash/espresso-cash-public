c7ce041fe5169aaeb52adc3d34554e27a053af35
Ricardo Brunetto <ricardo.brunetto@eurekalabs.com.br>
2022-09-26 17:33:13 -0300

    refactor: move jupiter client to cryptoplease_api package (#452)

--- cryptoplease_api/lib/cryptoplease_api.dart
+++ cryptoplease_api/lib/cryptoplease_api.dart
@@ -1,5 +1,7 @@
 library cryptoplease_api;
 
+export 'package:jupiter_aggregator/jupiter_aggregator.dart';
+
 export 'src/client.dart';
 export 'src/dto/add_funds.dart';
 export 'src/dto/create_payment.dart';
