c4b73eadd69ce0b3ec3df389db0017f66ffa88ce
ookami-kb <ookami.kb@gmail.com>
2022-10-02 22:46:29 +0200

    feat: add ODPModule

---
+++ packages/cryptoplease/lib/data/db/mixins.dart
@@ -0,0 +1,14 @@
+import 'package:drift/drift.dart';
+
+mixin AmountMixin on Table {
+  IntColumn get amount => integer()();
+  TextColumn get token => text()();
+}
+
+mixin EntityMixin on Table {
+  TextColumn get id => text()();
+  DateTimeColumn get created => dateTime()();
+
+  @override
+  Set<Column<dynamic>>? get primaryKey => {id};
+}
