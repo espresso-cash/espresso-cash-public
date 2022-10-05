15d184f51530dc0711a3cc2f05ed1a3d5b8d0a70
Ricardo Brunetto <ricardo@rhbrunetto.com>
2022-10-05 11:19:55 -0300

    add copy to clipboard address

--- packages/cryptoplease/lib/features/profile/profile_screen.dart
+++ packages/cryptoplease/lib/features/profile/profile_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:cryptoplease/config.dart';
 import 'package:cryptoplease/core/accounts/bl/account.dart';
+import 'package:cryptoplease/core/presentation/utils.dart';
 import 'package:cryptoplease/features/profile/components/profile_section.dart';
 import 'package:cryptoplease/features/qr_scanner/qr_address_data.dart';
 import 'package:cryptoplease_ui/cryptoplease_ui.dart';
@@ -27,79 +28,81 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final photoPath = state.photoPath;
     final address = state.address;
 
-    return Material(
-      color: Colors.white,
-      child: SafeArea(
-        bottom: false,
-        maintainBottomViewPadding: false,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(
-                  _buttonSpacing,
-                  _buttonSpacing,
-                  _buttonSpacing,
-                  0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: _imageSize,
-                      width: MediaQuery.of(context).size.width,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: CpUserAvatar(
-                              radius: _imageSize / 2,
-                              image: photoPath?.let(
-                                (it) => FileImage(File(it)),
+    return Scaffold(
+      body: Material(
+        color: Colors.white,
+        child: SafeArea(
+          bottom: false,
+          maintainBottomViewPadding: false,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    _buttonSpacing,
+                    _buttonSpacing,
+                    _buttonSpacing,
+                    0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: _imageSize,
+                        width: MediaQuery.of(context).size.width,
+                        child: Stack(
+                          children: [
+                            Center(
+                              child: CpUserAvatar(
+                                radius: _imageSize / 2,
+                                image: photoPath?.let(
+                                  (it) => FileImage(File(it)),
+                                ),
+                                userName: name,
                               ),
-                              userName: name,
                             ),
-                          ),
-                          Positioned(
-                            top: 0,
-                            right: 0,
-                            child: CpIconButton(
-                              icon: Icons.close_outlined,
-                              onPressed: Navigator.of(context).pop,
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: CpIconButton(
+                                icon: Icons.close_outlined,
+                                onPressed: Navigator.of(context).pop,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        name,
-                        style: Theme.of(context).textTheme.headline3,
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          name,
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 24),
-                    _QrCodeWidget(address: address, name: name),
-                  ],
+                      const SizedBox(height: 24),
+                      _QrCodeWidget(address: address, name: name),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16,
-                  horizontal: 24,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 24,
+                  ),
+                  child: Column(
+                    children: const [
+                      EditProfileSection(),
+                      DangerSection(),
+                      if (!isProd) DebugSection(),
+                      ShareSection(),
+                      VersionSection(),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  children: const [
-                    EditProfileSection(),
-                    DangerSection(),
-                    if (!isProd) DebugSection(),
-                    ShareSection(),
-                    VersionSection(),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -126,30 +129,33 @@ class _QrCodeWidget extends StatelessWidget {
       QrAddressData(address: address, name: name).toJson(),
     );
 
-    return Container(
-      height: 150,
-      alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-      decoration: const BoxDecoration(
-        color: CpColors.lightGreyBackground,
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      child: Row(
-        children: [
-          QrImage(data: qrData, padding: EdgeInsets.zero),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24),
-              child: Text(
-                address,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+    return InkWell(
+      onTap: () => context.copyToClipboard(address),
+      child: Container(
+        height: 150,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        decoration: const BoxDecoration(
+          color: CpColors.lightGreyBackground,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Row(
+          children: [
+            QrImage(data: qrData, padding: EdgeInsets.zero),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Text(
+                  address,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
