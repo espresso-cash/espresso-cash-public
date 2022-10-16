4b2fd47cd7c8844527b350797c180a2830dc1fee
Kirill Bubochkin <ookami.kb@gmail.com>
2022-10-16 21:15:20 +0200

    refactor: remove unused code (#529)

--- packages/cryptoplease/lib/app/screens/authenticated/receive_flow/routes.dart
+++ packages/cryptoplease/lib/app/screens/authenticated/receive_flow/routes.dart
@@ -1,11 +1,9 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:cryptoplease/app/screens/authenticated/receive_flow/request_type_screen.dart';
 import 'package:cryptoplease/app/screens/authenticated/receive_flow/share_address_screen.dart';
 import 'package:cryptoplease/features/payment_request/presentation/link_details/routes.dart';
 import 'package:cryptoplease/features/payment_request/presentation/link_request/routes.dart';
 
 const receiveFlowRoutes = [
-  AutoRoute<void>(page: RequestTypeScreen),
   AutoRoute<void>(page: ShareAddressScreen),
   linkRequestRoutes,
   ...linkDetailsRoutes,
