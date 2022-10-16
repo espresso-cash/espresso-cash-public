9e3b1684556135f7325e4accb01dd6d2d31def67
ookami-kb <ookami.kb@gmail.com>
2022-10-16 23:33:42 +0200

    refactor: merge storybook into cryptoplease

---
+++ packages/cryptoplease/lib/storybook/stories/status_widget.dart
@@ -0,0 +1,31 @@
+import 'package:cryptoplease/storybook/utils.dart';
+import 'package:cryptoplease/ui/status_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+final cpStatus = Story(
+  name: 'CpStatusWidget',
+  builder: (context) => Container(
+    height: 150,
+    padding: const EdgeInsets.all(16),
+    child: CpStatusWidget(
+      title: context.knobs.options(
+        label: 'Title',
+        initial: null,
+        options: const [
+          Option(label: 'With title', value: Text('Success!')),
+          Option(label: 'Empty', value: null),
+        ],
+      ),
+      content: Text(
+        context.knobs
+            .text(label: 'Content', initial: 'Your transfer has been sent.'),
+      ),
+      statusType: context.knobs.options(
+        label: 'Type',
+        initial: CpStatusType.success,
+        options: CpStatusType.values.toOptions(),
+      ),
+    ),
+  ),
+);
