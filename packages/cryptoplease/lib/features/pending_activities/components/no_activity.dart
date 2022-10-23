03f52d811e96a8f1c8c97a15aefe96a1718dfee5
ookami-kb <ookami.kb@gmail.com>
2022-10-23 22:40:40 +0200

    fix: NoActivity layout

--- packages/cryptoplease/lib/features/pending_activities/components/no_activity.dart
+++ packages/cryptoplease/lib/features/pending_activities/components/no_activity.dart
@@ -3,6 +3,7 @@ import 'package:cryptoplease/app/routes.dart';
 import 'package:cryptoplease/gen/assets.gen.dart';
 import 'package:cryptoplease/l10n/l10n.dart';
 import 'package:cryptoplease/ui/button.dart';
+import 'package:cryptoplease/ui/navigation_bar/navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class NoActivity extends StatelessWidget {
@@ -10,20 +11,23 @@ class NoActivity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(44),
+        padding: const EdgeInsets.symmetric(horizontal: 44),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            const Spacer(),
             Assets.images.logoIcon.image(height: 101),
             const SizedBox(height: 21),
             Text(context.l10n.noActivity),
-            const SizedBox(height: 105),
+            const Spacer(),
             CpButton(
               text: context.l10n.requestOrSendPayment,
               width: double.infinity,
               size: CpButtonSize.big,
               onPressed: () => context.router.navigate(const WalletFlowRoute()),
-            )
+            ),
+            const Spacer(),
+            const SizedBox(height: cpNavigationBarheight),
           ],
         ),
       );
