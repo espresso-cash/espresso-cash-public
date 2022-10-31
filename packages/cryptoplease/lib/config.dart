b2c2bdaee31f5fa85bec7920e3904c450d9f6966
Antoine Herzog <antoine.herzog@hey.com>
2022-10-31 18:25:31 +0100

    fix: add https for ftx url (#590)

--- packages/cryptoplease/lib/config.dart
+++ packages/cryptoplease/lib/config.dart
@@ -65,6 +65,6 @@ const moonpayHost = 'moonpay.$cpLinkDomain';
 const kadoBaseUrl = 'https://app.kado.money/';
 const kadoApiKey = String.fromEnvironment('KADO_API_KEY');
 const ftxAddCashSupportUrl =
-    'http://www.espressocash.com/docs/using-the-app/add-cash-ftx-com';
+    'https://www.espressocash.com/docs/using-the-app/add-cash-ftx-com';
 const ftxCashoutSupportUrl =
-    'http://www.espressocash.com/docs/using-the-app/cash-out-ftx-com';
+    'https://www.espressocash.com/docs/using-the-app/cash-out-ftx-com';
