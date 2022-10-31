20c0a71899248cf5d9d620ac4e4fc3fdfa0085a6
Ricardo Brunetto <ricardo@rhbrunetto.com>
2022-10-30 22:19:19 -0300

    upd ui lib

--- packages/cryptoplease/lib/ui/tab_bar.dart
+++ packages/cryptoplease/lib/ui/tab_bar.dart
@@ -2,30 +2,73 @@ import 'package:flutter/material.dart';
 
 import 'colors.dart';
 
-class CpTabBar extends StatelessWidget {
-  const CpTabBar({super.key, required this.tabs});
+enum CpTabBarVariant { dark, inverted }
 
+class CpTabBar extends StatelessWidget {
+  const CpTabBar({
+    super.key,
+    this.variant = CpTabBarVariant.dark,
+    required this.tabs,
+  });
+
+  final CpTabBarVariant variant;
   final List<Widget> tabs;
+
+  Color get _backgroundColor {
+    switch (variant) {
+      case CpTabBarVariant.dark:
+        return CpColors.darkBackground;
+      case CpTabBarVariant.inverted:
+        return CpColors.yellowColor;
+    }
+  }
+
+  Color get _indicatorBackground {
+    switch (variant) {
+      case CpTabBarVariant.dark:
+        return Colors.white;
+      case CpTabBarVariant.inverted:
+        return Colors.black;
+    }
+  }
+
+  Color get _labelColor {
+    switch (variant) {
+      case CpTabBarVariant.dark:
+        return Colors.black;
+      case CpTabBarVariant.inverted:
+        return Colors.white;
+    }
+  }
+
+  Color get _unselectedLabelColor {
+    switch (variant) {
+      case CpTabBarVariant.dark:
+        return CpColors.disabledTabColor;
+      case CpTabBarVariant.inverted:
+        return Colors.black;
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Container(
         height: 50,
         clipBehavior: Clip.antiAlias,
-        decoration: const ShapeDecoration(
-          color: CpColors.darkBackground,
-          shape: StadiumBorder(),
+        decoration: ShapeDecoration(
+          color: _backgroundColor,
+          shape: const StadiumBorder(),
         ),
         child: TabBar(
           indicatorColor: Colors.transparent,
-          unselectedLabelColor: CpColors.disabledTabColor,
-          labelColor: Colors.black,
+          unselectedLabelColor: _unselectedLabelColor,
+          labelColor: _labelColor,
           labelStyle: Theme.of(context).textTheme.headline3?.copyWith(
                 fontSize: 13,
                 fontWeight: FontWeight.w500,
               ),
-          indicator: const ShapeDecoration(
-            color: Colors.white,
-            shape: StadiumBorder(),
+          indicator: ShapeDecoration(
+            color: _indicatorBackground,
+            shape: const StadiumBorder(),
           ),
           padding: const EdgeInsets.all(6),
           tabs: tabs,
