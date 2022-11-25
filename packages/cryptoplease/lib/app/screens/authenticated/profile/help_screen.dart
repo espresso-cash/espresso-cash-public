a4fd72c426837b4cdae460be5c7e147e7197abcb
Justin Enerio <justinenerio@pm.me>
2022-11-26 00:28:40 +0700

    feat: add Intercom support (#633)
    Co-authored-by: ookami-kb <ookami.kb@gmail.com>

--- packages/cryptoplease/lib/app/screens/authenticated/profile/help_screen.dart
+++ packages/cryptoplease/lib/app/screens/authenticated/profile/help_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../../../../config.dart';
 import '../../../../core/presentation/utils.dart';
+import '../../../../features/intercom/module.dart';
 import '../../../../l10n/l10n.dart';
 import '../../../../ui/app_bar.dart';
-import 'components/help_bottom_sheet.dart';
 import 'components/profile_button.dart';
 
 class HelpScreen extends StatelessWidget {
@@ -26,8 +26,8 @@ class HelpScreen extends StatelessWidget {
                 onPressed: () => context.openLink(faqUrl),
               ),
               ProfileButton(
-                label: context.l10n.sendUsEmail,
-                onPressed: () => HelpBottomSheet.show(context),
+                label: context.l10n.contactUs,
+                onPressed: displayIntercomMessenger,
               ),
             ],
           ),
