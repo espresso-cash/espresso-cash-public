a00c65fd0b9e7094b29bf7c27154907bd44b86ba
Justin Enerio <justinenerio@pm.me>
2022-11-29 20:00:25 +0800

    fix: hide tokens based on fiat balance

--- packages/cryptoplease/lib/features/investments/src/presentation/my_portfolio.dart
+++ packages/cryptoplease/lib/features/investments/src/presentation/my_portfolio.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/balances/bl/balances_bloc.dart';
+import '../../../../core/balances/presentation/watch_balance.dart';
 import '../data/repository.dart';
 import 'components/portfolio_widget.dart';
+
+const minimumUsdAmount = 0.01;
 
 class MyPortfolio extends StatelessWidget {
   const MyPortfolio({super.key});
@@ -17,11 +20,20 @@ class MyPortfolio extends StatelessWidget {
               .watch<InvestmentSettingsRepository>()
               .displayEmptyBalances;
 
+          final zeroAmountTokens = state.balances.keys
+              .where(
+                (token) =>
+                    (context.watchUserFiatBalance(token)?.value ?? 0) <
+                    minimumUsdAmount,
+              )
+              .toList();
+
           return PortfolioWidget(
             tokens: IList(
               displayEmptyBalances
                   ? state.userTokens
-                  : state.userTokensWithPositiveBalance,
+                  : state.userTokens
+                      .where((token) => !zeroAmountTokens.contains(token)),
             ),
           );
         },
