1148428dec12f8bc046c23aeeaddd5f816f2f270
Justin Enerio <justinenerio@pm.me>
2022-12-12 12:54:26 +0800

    fixed tests

--- packages/cryptoplease/test/features/qr_scanner/qr_scanner_bloc_test.dart
+++ packages/cryptoplease/test/features/qr_scanner/qr_scanner_bloc_test.dart
@@ -1,10 +1,13 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:cryptoplease/core/link_shortener.dart';
+import 'package:cryptoplease/core/tip_payments.dart';
+import 'package:cryptoplease/core/tokens/token.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_address_data.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_scanner_bloc.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_scanner_request.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:solana/solana.dart';
 
 import 'qr_scanner_bloc_test.mocks.dart';
@@ -16,6 +19,8 @@ void main() {
 
   setUpAll(() async {
     sampleAddress = (await Ed25519HDKeyPair.random()).address;
+
+    when(linkShortener.reverse(any)).thenAnswer((_) async => null);
   });
 
   group('QrScannerBloc tests:', () {
@@ -52,6 +57,21 @@ void main() {
         ),
       ],
     );
+
+    blocTest<QrScannerBloc, QrScannerState>(
+      'tip link scanned',
+      build: () => QrScannerBloc(linkShortener: linkShortener),
+      setUp: () => when(linkShortener.reverse(any))
+          .thenAnswer((_) async => _buildTipLink()),
+      act: (bloc) => bloc.add(const QrScannerEvent.received('mocked')),
+      expect: () => [
+        QrScannerState.done(
+          QrScannerRequest.tip(
+            TipPaymentData(key: 'abcd', token: Token.usdc.publicKey),
+          ),
+        ),
+      ],
+    );
   });
 }
 
@@ -72,3 +92,6 @@ String _buildSolanaPayURI({
 
   return 'solana:$recipientAddress?${values.join('&')}';
 }
+
+Uri _buildTipLink() =>
+    TipPaymentData(key: 'abcd', token: Token.usdc.publicKey).toUri();
