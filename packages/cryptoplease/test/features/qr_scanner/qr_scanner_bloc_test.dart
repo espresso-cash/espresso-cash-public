2fcb25bf05c6028b1809c50a8b1f2339a4094b01
Justin Enerio <justinenerio@pm.me>
2022-12-12 11:38:29 +0800

    fix tests

--- packages/cryptoplease/test/features/qr_scanner/qr_scanner_bloc_test.dart
+++ packages/cryptoplease/test/features/qr_scanner/qr_scanner_bloc_test.dart
@@ -1,11 +1,17 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:cryptoplease/core/link_shortener.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_address_data.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_scanner_bloc.dart';
 import 'package:cryptoplease/features/qr_scanner/src/qr_scanner_request.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:solana/solana.dart';
 
+import 'qr_scanner_bloc_test.mocks.dart';
+
+@GenerateMocks([LinkShortener])
 void main() {
+  final linkShortener = MockLinkShortener();
   late final String sampleAddress;
 
   setUpAll(() async {
@@ -15,14 +21,14 @@ void main() {
   group('QrScannerBloc tests:', () {
     blocTest<QrScannerBloc, QrScannerState>(
       'bloc can be correctly initialized',
-      build: QrScannerBloc.new,
+      build: () => QrScannerBloc(linkShortener: linkShortener),
       act: (bloc) => bloc.add(const QrScannerEvent.initialized()),
       expect: () => [const QrScannerState.initial()],
     );
 
     blocTest<QrScannerBloc, QrScannerState>(
       'solana pay QR code leads to done state',
-      build: QrScannerBloc.new,
+      build: () => QrScannerBloc(linkShortener: linkShortener),
       act: (bloc) => bloc.add(
         QrScannerEvent.received(
           _buildSolanaPayURI(recipientAddress: sampleAddress),
@@ -33,7 +39,7 @@ void main() {
 
     blocTest<QrScannerBloc, QrScannerState>(
       'simple address qr scanned',
-      build: QrScannerBloc.new,
+      build: () => QrScannerBloc(linkShortener: linkShortener),
       act: (bloc) => bloc.add(QrScannerEvent.received(sampleAddress)),
       expect: () => [
         QrScannerState.done(
