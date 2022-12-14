f89cd416d697141ad15b76de1ca2875a78294bf9
Ricardo Brunetto <ricardo.brunetto@eurekalabs.com.br>
2022-12-14 13:45:13 -0300

    feat: add buy and sell flows to app (#537)
    Co-authored-by: ookami-kb <ookami.kb@gmail.com>

--- packages/cryptoplease/lib/data/db/db.dart
+++ packages/cryptoplease/lib/data/db/db.dart
@@ -7,6 +7,7 @@ import '../../features/incoming_split_key_payments/module.dart';
 import '../../features/outgoing_direct_payments/module.dart';
 import '../../features/outgoing_split_key_payments/module.dart';
 import '../../features/payment_request/module.dart';
+import '../../features/swap/module.dart';
 import 'open_connection.dart';
 
 part 'db.g.dart';
@@ -20,7 +21,7 @@ class OutgoingTransferRows extends Table {
   Set<Column<dynamic>>? get primaryKey => {id};
 }
 
-const int latestVersion = 20;
+const int latestVersion = 21;
 
 const _tables = [
   OutgoingTransferRows,
@@ -28,6 +29,7 @@ const _tables = [
   ODPRows,
   OSKPRows,
   ISKPRows,
+  SwapRows,
   TransactionRows,
 ];
 
@@ -78,9 +80,11 @@ class MyDatabase extends _$MyDatabase {
           if (from >= 16 && from < 19) {
             await m.addColumn(oSKPRows, oSKPRows.txFailureReason);
           }
-
           if (from < 20) {
             await m.createTable(transactionRows);
+          }
+          if (from < 21) {
+            await m.createTable(swapRows);
           }
         },
       );
