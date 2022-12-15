6a22173bacb817a2f1ddcec4898ecd02046d873b
Justin Enerio <justinenerio@pm.me>
2022-12-15 21:47:53 +0800

    upd

--- packages/cryptoplease/lib/features/popular_tokens/src/bl/repository.dart
+++ packages/cryptoplease/lib/features/popular_tokens/src/bl/repository.dart
@@ -1,9 +1,11 @@
+import 'package:collection/collection.dart';
 import 'package:dfunc/dfunc.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../../config.dart';
 import '../../../../core/tokens/token.dart';
+import '../../../../core/tokens/token_list.dart';
 import '../data/coingecko_client.dart';
 import '../data/popular_token_cache.dart';
 
@@ -12,11 +14,14 @@ class PopularTokenRepository {
   PopularTokenRepository({
     required MarketsCoingeckoClient coingeckoClient,
     required PopularTokenCache popularTokenCache,
+    required TokenList tokenList,
   })  : _coingeckoClient = coingeckoClient,
-        _cache = popularTokenCache;
+        _cache = popularTokenCache,
+        _tokenList = tokenList;
 
   final MarketsCoingeckoClient _coingeckoClient;
   final PopularTokenCache _cache;
+  final TokenList _tokenList;
 
   AsyncResult<IMap<Token, double>> get({
     required String currency,
@@ -37,14 +42,22 @@ class PopularTokenRepository {
   }) async =>
       _coingeckoClient
           .getMarketTokens(
-            MarketsRequestDto(vsCurrency: currency, perPage: noOfTokens),
+            MarketsRequestDto(
+              vsCurrency: currency,
+              perPage: noOfTokens,
+            ),
           )
           .toEither()
           .mapAsync(
+            (responses) => responses.map(
+              (r) => r
+                  .toToken(_tokenList)
+                  .let((t) => MapEntry(t, r.currentPrice ?? 0)),
+            ),
+          )
+          .mapAsync(
         (responses) {
-          final res = IMap(
-            {for (var e in responses) e.fromCoingecko(): e.currentPrice ?? 0},
-          );
+          final res = IMap.fromEntries(responses);
 
           _cache.set(res);
 
@@ -56,7 +69,21 @@ class PopularTokenRepository {
 }
 
 extension on MarketsResponseDto {
-  Token fromCoingecko() => Token(
+  Token toToken(TokenList tokenList) {
+    final id = this.id;
+    final symbol = this.symbol?.toLowerCase();
+
+    if (id == null) return _createStubToken();
+    if (symbol == Token.sol.symbol.toLowerCase()) return Token.sol;
+
+    return tokenList.tokens
+        .singleWhereOrNull(
+          (t) => t.symbol.toLowerCase() == symbol && t.coingeckoId == id,
+        )
+        .ifNull(_createStubToken);
+  }
+
+  Token _createStubToken() => Token(
         chainId: currentChainId,
         address: id ?? '',
         symbol: symbol?.toUpperCase() ?? '',
