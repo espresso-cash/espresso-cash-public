22c84813fce49e5a07a516e209f4f30fde4e5212
Justin Enerio <justinenerio@pm.me>
2022-12-15 14:39:59 +0800

    wip

--- packages/cryptoplease/lib/features/favorite_tokens/src/bl/repository.dart
+++ packages/cryptoplease/lib/features/favorite_tokens/src/bl/repository.dart
@@ -1,14 +1,23 @@
+import 'package:collection/collection.dart';
+import 'package:dfunc/dfunc.dart';
 import 'package:drift/drift.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../../../config.dart';
 import '../../../../core/tokens/token.dart';
+import '../../../../core/tokens/token_list.dart';
 import '../../../../data/db/db.dart';
 
 @injectable
 class FavoriteTokenRepository {
-  FavoriteTokenRepository(this._db);
+  FavoriteTokenRepository({
+    required MyDatabase database,
+    required TokenList tokenList,
+  })  : _db = database,
+        _tokenList = tokenList;
 
   final MyDatabase _db;
+  final TokenList _tokenList;
 
   Future<void> add(Token token) async {
     await _db.into(_db.favoriteTokenRows).insert(token.toDto());
@@ -24,13 +33,17 @@ class FavoriteTokenRepository {
   Stream<List<Token>> watch() {
     final query = _db.select(_db.favoriteTokenRows);
 
-    return query.watch().map((e) => e.map((e) => e.toModel()).toList());
+    return query
+        .watch()
+        .map((e) => e.map((e) => e.toToken(_tokenList)).toList());
   }
 
   Future<List<Token>> fetch() async {
     final query = _db.select(_db.favoriteTokenRows);
 
-    return query.get().then((e) => e.map((e) => e.toModel()).toList());
+    return query
+        .get()
+        .then((e) => e.map((e) => e.toToken(_tokenList)).toList());
   }
 
   Future<bool> isFavorite(Token token) {
@@ -65,14 +78,27 @@ extension on Token {
 }
 
 extension on FavoriteTokenRow {
-  Token toModel() => Token(
-        name: name,
-        symbol: symbol,
-        logoURI: logoUri,
-        extensions: Extensions(coingeckoId: id),
+  Token toToken(TokenList tokenList) {
+    final id = this.id;
+    final symbol = this.symbol.toLowerCase();
+
+    if (symbol == Token.sol.symbol.toLowerCase()) return Token.sol;
+
+    return tokenList.tokens
+        .singleWhereOrNull(
+          (t) => t.symbol.toLowerCase() == symbol && t.coingeckoId == id,
+        )
+        .ifNull(_createStubToken);
+  }
+
+  Token _createStubToken() => Token(
+        chainId: currentChainId,
         address: id,
-        chainId: 0,
+        symbol: symbol.toUpperCase(),
+        name: name,
         decimals: 0,
+        logoURI: logoUri,
         tags: const [],
+        extensions: Extensions(coingeckoId: id),
       );
 }
