ee422556967e088fa9b0442819f6b390a754857c
ookami-kb <ookami.kb@gmail.com>
2023-01-12 22:24:53 +0100

    гзв

--- packages/cryptoplease/lib/app/screens/authenticated/routes.dart
+++ packages/cryptoplease/lib/app/screens/authenticated/routes.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:auto_route/empty_router_widgets.dart';
 
 import '../../../features/app_lock/routes.dart';
 import '../../../features/backup_phrase/routes.dart';
