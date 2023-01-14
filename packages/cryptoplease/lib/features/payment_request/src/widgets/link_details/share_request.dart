34e91930e5e5a15048445aab32b01629a6baa584
ookami-kb <ookami.kb@gmail.com>
2023-01-14 12:14:06 +0100

    upd

--- packages/cryptoplease/lib/features/payment_request/src/widgets/link_details/share_request.dart
+++ packages/cryptoplease/lib/features/payment_request/src/widgets/link_details/share_request.dart
@@ -2,13 +2,14 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../../di.dart';
 import '../../../../../l10n/l10n.dart';
 import '../../../../../ui/app_bar.dart';
 import '../../../../../ui/tab_bar.dart';
 import '../../../../../ui/text_button.dart';
 import '../../../../../ui/theme.dart';
-import '../../../delete_payment.dart';
 import '../../../models/payment_request.dart';
+import '../../bl/repository.dart';
 import 'components/share_link.dart';
 import 'components/share_qr_code.dart';
 
@@ -59,7 +60,7 @@ class SharePaymentRequestScreen extends StatelessWidget {
                 child: CpTextButton(
                   text: context.l10n.paymentRequestCancel,
                   onPressed: () {
-                    deletePaymentRequest(request.id);
+                    sl<PaymentRequestRepository>().delete(request.id);
                     context.router.pop();
                   },
                 ),
