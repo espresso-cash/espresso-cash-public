049c6bc80053e9276fc942804580679614e80e47
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-01-20 17:15:45 -0300

    fix: use env for solana rpc url on solana pay

--- packages/cryptoplease_link/lib/src/handlers/solana_handler.dart
+++ packages/cryptoplease_link/lib/src/handlers/solana_handler.dart
@@ -5,13 +5,14 @@ import 'package:shelf/shelf.dart';
 
 Future<Response> Function(Request) createSolanaHandler({
   required Map<String, String> tokens,
+  required String network,
 }) =>
     (Request request) async {
       final host = request.requestedUri.host;
       if (host == link1Host || host == link2Host) {
         return _solanaShareableHandler(request);
       } else if (host == solanaPayHost) {
-        return _solanaPayHandler(request, tokens);
+        return _solanaPayHandler(request, tokens, network);
       }
 
       return Response.notFound(null);
@@ -26,6 +27,7 @@ Future<Response> _solanaShareableHandler(Request request) => commonHandler(
 Future<Response> _solanaPayHandler(
   Request request,
   Map<String, String> tokens,
+  String network,
 ) {
   final tokenParam = request.url.queryParameters['spl-token'];
   final token = tokenParam == null ? 'SOL' : tokens[tokenParam] ?? '';
@@ -39,6 +41,7 @@ Future<Response> _solanaPayHandler(
       ...data,
       'token': token,
       'amount': amount,
+      'network': network,
       'recipient': request.url.path.replaceAll('/', ''),
     },
   );
--- packages/cryptoplease_link/public/scripts/phantom-app.js
+++ packages/cryptoplease_link/public/scripts/phantom-app.js
@@ -2,42 +2,53 @@ const checkPhantom = function () {
   const isPhantomInstalled = window.solana && window.solana.isPhantom;
 
   if (isPhantomInstalled) {
-    document.getElementById('phantom-button').style.visibility = 'visible';
+    document.getElementById("phantom-button").style.visibility = "visible";
   } else {
-    document.getElementById('phantom-button').style.visibility = 'hidden';
+    document.getElementById("phantom-button").style.visibility = "hidden";
   }
 };
 
-const sendTransaction = function (recipient) {
+const sendTransaction = function (network, recipient) {
   window.solana
-  .connect()
-  .then(({ publicKey }) => _createAndSendTransaction(publicKey, recipient))
-  .catch(_onError);
+    .connect()
+    .then(({ publicKey }) =>
+      _createAndSendTransaction(network, publicKey, recipient)
+    )
+    .catch(_onError);
 };
 
 const _onError = console.warn;
 
 const _buildSolanaPayParameters = async (recipient) => {
-  const protocol = 'solana:'
+  const protocol = "solana:";
   const params = window.location.search;
-  const uri = protocol + recipient + params
-  return solanaPay.parseURL(uri)
-}
+  const uri = protocol + recipient + params;
+  return solanaPay.parseURL(uri);
+};
 
 const _createTransfer = async (connection, publicKey, recipient) => {
   if (!publicKey) return;
 
-  const data = await _buildSolanaPayParameters(recipient)
-  const transaction = await solanaPay.createTransfer(connection, publicKey, data);
+  const data = await _buildSolanaPayParameters(recipient);
+  const transaction = await solanaPay.createTransfer(
+    connection,
+    publicKey,
+    data
+  );
   transaction.feePayer = publicKey;
   const anyTransaction = transaction;
-  anyTransaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+  anyTransaction.recentBlockhash = (
+    await connection.getRecentBlockhash()
+  ).blockhash;
 
   return transaction;
 };
 
-const _createAndSendTransaction = async function (publicKey, recipient) {
-  const network = 'https://api.mainnet-beta.solana.com';
+const _createAndSendTransaction = async function (
+  network,
+  publicKey,
+  recipient
+) {
   const connection = new solanaWeb3.Connection(network);
 
   try {
@@ -46,9 +57,9 @@ const _createAndSendTransaction = async function (publicKey, recipient) {
     const provider = window.solana;
     await provider.signAndSendTransaction(transaction);
 
-    alert('Payment has been sent successfully!');
+    alert("Payment has been sent successfully!");
   } catch (err) {
     console.warn(err);
-    alert('Error sending the transaction!');
+    alert("Error sending the transaction!");
   }
 };
