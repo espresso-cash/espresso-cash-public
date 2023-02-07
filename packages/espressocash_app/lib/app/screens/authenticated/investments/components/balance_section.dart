fc6b6ee4ccf5e3e09e83d29ae5c1be1d7eff5ecb
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-02-07 18:45:51 -0300

    wip

---
+++ packages/espressocash_app/lib/app/screens/authenticated/investments/components/balance_section.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/amount.dart';
+import '../../../../../core/balances/presentation/watch_balance.dart';
+import '../../../../../core/currency.dart';
+import '../../../../../core/presentation/format_amount.dart';
+import '../../../../../core/tokens/token.dart';
+import '../../../../../l10n/device_locale.dart';
+import '../../../../../l10n/l10n.dart';
+import '../../../../../ui/colors.dart';
+import '../../../../../ui/info_icon.dart';
+import '../../../../../ui/token_icon.dart';
+
+class BalanceSection extends StatefulWidget {
+  const BalanceSection({
+    Key? key,
+    required this.padding,
+  }) : super(key: key);
+
+  final EdgeInsets padding;
+
+  @override
+  State<BalanceSection> createState() => BalanceSectionState();
+}
+
+class BalanceSectionState extends State<BalanceSection> {
+  bool _showMore = false;
+
+  void _toggleInfo() => setState(() => _showMore = !_showMore);
+
+  @override
+  Widget build(BuildContext context) => AnimatedCrossFade(
+        crossFadeState:
+            _showMore ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+        duration: const Duration(milliseconds: 200),
+        firstChild: _Balance(
+          onInfoPressed: _toggleInfo,
+          padding: widget.padding,
+        ),
+        secondChild: _Info(
+          onClosePressed: _toggleInfo,
+          padding: widget.padding,
+        ),
+        layoutBuilder: _crossFadeBuilder,
+      );
+}
+
+class _Balance extends StatelessWidget {
+  const _Balance({
+    Key? key,
+    required this.onInfoPressed,
+    required this.padding,
+  }) : super(key: key);
+
+  final VoidCallback onInfoPressed;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) {
+    const token = Token.usdc;
+
+    final locale = DeviceLocale.localeOf(context);
+    final converted = context.watchUserFiatBalance(token);
+    final amount = converted ?? Amount.zero(currency: Currency.usd);
+    final formattedAmount = amount.format(locale);
+
+    final clickable = GestureDetector(
+      onTap: onInfoPressed,
+      child: Text.rich(
+        TextSpan(
+          text: context.l10n.inUsdc,
+          children: const [
+            WidgetSpan(
+              alignment: PlaceholderAlignment.middle,
+              child: SizedBox.square(
+                dimension: 18,
+                child: _InfoIcon(),
+              ),
+            )
+          ],
+        ),
+        style: _baseStyle.copyWith(color: CpColors.yellowColor),
+      ),
+    );
+
+    return Center(
+      child: Padding(
+        padding: padding,
+        child: Row(
+          children: [
+            Flexible(
+              child: FittedBox(
+                child: Text.rich(
+                  TextSpan(
+                    text: context.l10n.cryptoCashBalance,
+                    children: [
+                      WidgetSpan(child: clickable),
+                    ],
+                  ),
+                  style: _baseStyle,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Flexible(
+              child: FittedBox(
+                child: Text(
+                  formattedAmount,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 50,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            const CpTokenIcon(token: token, size: 36),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Info extends StatelessWidget {
+  const _Info({
+    Key? key,
+    required this.onClosePressed,
+    required this.padding,
+  }) : super(key: key);
+
+  final VoidCallback onClosePressed;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          // TODO(rhbrunetto): Fix it, wrong icon style
+          CloseButton(onPressed: onClosePressed, color: Colors.white),
+          Padding(
+            padding: padding,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox.square(
+                  dimension: 28,
+                  child: _InfoIcon(),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    context.l10n.usdcInfo,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.5,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ),
+        ],
+      );
+}
+
+class _InfoIcon extends StatelessWidget {
+  const _InfoIcon();
+
+  @override
+  Widget build(BuildContext context) => Container(
+        padding: const EdgeInsets.all(2),
+        decoration: const ShapeDecoration(
+          shape: CircleBorder(),
+          color: CpColors.yellowColor,
+        ),
+        child: const CpInfoIcon(iconColor: CpColors.darkBackground),
+      );
+}
+
+const _baseStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.2,
+);
+
+Widget _crossFadeBuilder(
+  Widget topChild,
+  Key topChildKey,
+  Widget bottomChild,
+  Key bottomChildKey,
+) =>
+    Stack(
+      clipBehavior: Clip.none,
+      children: <Widget>[
+        Positioned(
+          key: bottomChildKey,
+          left: 0.0,
+          top: 0.0,
+          right: 0.0,
+          bottom: 0.0,
+          child: bottomChild,
+        ),
+        Positioned(
+          key: topChildKey,
+          child: topChild,
+        ),
+      ],
+    );
--- packages/espressocash_app/lib/app/screens/authenticated/investments/components/investment_header.dart
+++ packages/espressocash_app/lib/app/screens/authenticated/investments/components/investment_header.dart
@@ -1,155 +1,29 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/amount.dart';
-import '../../../../../core/balances/presentation/watch_balance.dart';
-import '../../../../../core/currency.dart';
-import '../../../../../core/presentation/format_amount.dart';
-import '../../../../../core/tokens/token.dart';
 import '../../../../../features/ramp/widgets/ramp_buttons.dart';
-import '../../../../../l10n/device_locale.dart';
-import '../../../../../l10n/l10n.dart';
-import '../../../../../routes.gr.dart';
-import '../../../../../ui/colors.dart';
-import '../../../../../ui/info_icon.dart';
-import '../../../../../ui/token_icon.dart';
+import 'balance_section.dart';
 
 class InvestmentHeader extends StatelessWidget {
   const InvestmentHeader({super.key});
 
   @override
-  Widget build(BuildContext context) => DecoratedBox(
-        decoration: const BoxDecoration(color: Color(0xff2D2B2C)),
-        child: Column(
-          children: const [
-            _Info(),
-            _Balance(),
-            Divider(color: Color(0xff4B4B4B), height: 8),
-            Padding(
-              padding: EdgeInsets.only(bottom: 16, top: 8.0),
-              child: RampButtons(),
-            ),
-          ],
-        ),
-      );
-}
-
-class _Info extends StatefulWidget {
-  const _Info();
-
-  @override
-  State<_Info> createState() => _InfoState();
-}
-
-class _InfoState extends State<_Info> {
-  bool _showMore = false;
-
-  @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-        child: SizedBox(
-          height: 55,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (!_showMore)
-                Expanded(
-                  child: Text(
-                    context.l10n.cryptoCashBalance,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.2,
-                    ),
-                  ),
-                )
-              else
-                Expanded(
-                  child: Text(
-                    context.l10n.usdcInfo,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.5,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              const SizedBox(width: 8),
-              _InfoIcon(
-                onTap: () {
-                  setState(() => _showMore = !_showMore);
-                },
-                isTapped: _showMore,
-              ),
-            ],
-          ),
-        ),
-      );
-}
-
-class _InfoIcon extends StatelessWidget {
-  const _InfoIcon({
-    required this.onTap,
-    required this.isTapped,
-  });
-  final VoidCallback onTap;
-  final bool isTapped;
-
-  @override
-  Widget build(BuildContext context) => InkWell(
-        onTap: onTap,
-        child: Container(
-          height: 28,
-          width: 28,
-          padding: const EdgeInsets.all(5),
-          decoration: ShapeDecoration(
-            shape: const CircleBorder(),
-            color: isTapped ? const Color(0xff737273) : CpColors.yellowColor,
-          ),
-          child: const CpInfoIcon(
-            iconColor: Color(0xff2D2B2C),
-          ),
-        ),
-      );
-}
-
-class _Balance extends StatelessWidget {
-  const _Balance();
-
-  @override
   Widget build(BuildContext context) {
-    const token = Token.usdc;
+    const insets = EdgeInsets.symmetric(horizontal: 24);
 
-    final locale = DeviceLocale.localeOf(context);
-    final converted = context.watchUserFiatBalance(token);
-    final amount = converted ?? Amount.zero(currency: Currency.usd);
-    final formattedAmount = amount.format(locale);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: ListTile(
-        onTap: () => context.router.push(TokenDetailsRoute(token: token)),
-        leading: const CpTokenIcon(token: token, size: 36),
-        horizontalTitleGap: 8,
-        title: Text(
-          token.name,
-          style: const TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 15,
-            color: Colors.white,
+    return DecoratedBox(
+      decoration: const BoxDecoration(color: Color(0xff2D2B2C)),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 130,
+            child: BalanceSection(padding: insets),
           ),
-          overflow: TextOverflow.ellipsis,
-        ),
-        trailing: Text(
-          formattedAmount,
-          style: const TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 50,
-            color: Colors.white,
+          Divider(color: Color(0xff4B4B4B), height: 8),
+          Padding(
+            padding: EdgeInsets.only(bottom: 16, top: 8.0),
+            child: RampButtons(),
           ),
-        ),
+        ],
       ),
     );
   }
