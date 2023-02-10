f53404b44009148b082d93ff3dd0175d1f8b486a
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-02-10 15:46:02 -0300

    upd

--- packages/espressocash_app/lib/storybook/stories/header.dart
+++ packages/espressocash_app/lib/storybook/stories/header.dart
@@ -4,9 +4,12 @@ import '../../ui/header.dart';
 
 final cpHeader = Story(
   name: 'CpHeader',
-  builder: (context) => const CpHeader(
-    title: 'Popular Crypto',
-    subtitle:
-        'A fast and easy way to buy and sell\ncrypto anywhere in the world.',
+  builder: (context) => CpHeader(
+    title: context.knobs.text(label: 'Title', initial: 'Popular Crypto'),
+    subtitle: context.knobs.text(
+      label: 'Subtitle',
+      initial:
+          'A fast and easy way to buy and sell\ncrypto anywhere in the world.',
+    ),
   ),
 );
