c47dbfeb88b29ef7ec30c836ef1e4e6970659258
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-02-14 13:48:48 -0300

    wip

--- packages/espressocash_app/lib/ui/timeline.dart
+++ packages/espressocash_app/lib/ui/timeline.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:dfunc/dfunc.dart';
 import 'package:flutter/material.dart';
 
 import 'colors.dart';
@@ -46,13 +50,17 @@ class CpTimeline extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  _Indicator(
-                    color:
-                        index > active ? Colors.white : CpColors.darkBackground,
+                  _Border(
                     backgroundColor: status.backgroundColor,
                     isFirst: isFirst,
                     isLast: isLast,
-                    icon: index == active ? status.icon : null,
+                    child: _Indicator(
+                      color: index > active
+                          ? Colors.white
+                          : CpColors.darkBackground,
+                      shouldBounce: active == index,
+                      icon: index == active ? status.icon : null,
+                    ),
                   ),
                   if (!isLast)
                     _Connector(
@@ -60,6 +68,7 @@ class CpTimeline extends StatelessWidget {
                           ? Colors.white
                           : CpColors.darkBackground,
                       backgroundColor: status.backgroundColor,
+                      shouldBounce: index < active,
                     ),
                 ],
               ),
@@ -82,7 +91,7 @@ class _TileInfo extends StatelessWidget {
     final subtitle = tile.subtitle;
 
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -103,49 +112,194 @@ class _TileInfo extends StatelessWidget {
   }
 }
 
-class _Connector extends StatelessWidget {
+class _Connector extends StatefulWidget {
   const _Connector({
     required this.color,
     required this.backgroundColor,
+    required this.shouldBounce,
   });
 
   final Color color;
   final Color backgroundColor;
+  final bool shouldBounce;
+
+  @override
+  State<_Connector> createState() => _ConnectorState();
+}
+
+class _ConnectorState extends State<_Connector>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: _completeDuration);
+    _updateAnimation();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant _Connector oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _updateAnimation();
+  }
+
+  void _updateAnimation() {
+    if (!mounted) return;
+    if (!widget.shouldBounce) {
+      _controller.reset();
+    } else {
+      _controller.repeat();
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Container(
         width: _timelineWidth,
         height: _connectorHeight,
-        decoration: BoxDecoration(color: backgroundColor),
-        child: Center(
-          child: Container(
-            width: _connectorWidth,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: const BorderRadius.vertical(
-                top: _timelineRadius,
-                bottom: _timelineRadius,
+        decoration: BoxDecoration(color: widget.backgroundColor),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) => Positioned(
+                top: _controller.value
+                    .let(Curves.linear.transform)
+                    .let(_transformConnector),
+                child: child!,
+              ),
+              child: Container(
+                height: _connectorHeight,
+                width: _connectorWidth,
+                color: widget.color,
               ),
             ),
-          ),
+          ],
         ),
       );
 }
 
-class _Indicator extends StatelessWidget {
+class _Indicator extends StatefulWidget {
   const _Indicator({
     required this.color,
-    required this.backgroundColor,
-    required this.isFirst,
-    required this.isLast,
+    required this.shouldBounce,
     this.icon,
   });
 
   final Color color;
-  final Color backgroundColor;
-  final bool isFirst;
-  final bool isLast;
   final Widget? icon;
+  final bool shouldBounce;
+
+  @override
+  State<_Indicator> createState() => _IndicatorState();
+}
+
+class _IndicatorState extends State<_Indicator>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: _bounceDuration)
+      ..addStatusListener(_onAnimationStatusUpdate);
+    Future<void>.delayed(
+      Duration(
+        milliseconds: (_completeDuration.inMilliseconds * 0.25).toInt() -
+            _bounceDuration.inMilliseconds,
+      ),
+    ).then(_updateAnimation);
+  }
+
+  @override
+  void dispose() {
+    _controller
+      ..removeStatusListener(_onAnimationStatusUpdate)
+      ..dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant _Indicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _updateAnimation();
+  }
+
+  void _updateAnimation([_]) {
+    if (!mounted) return;
+    if (!widget.shouldBounce) return _controller.reset();
+    if (_controller.isAnimating) return;
+
+    if (_controller.isCompleted) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
+  }
+
+  void _onAnimationStatusUpdate(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      _updateAnimation();
+    } else if (status == AnimationStatus.dismissed) {
+      final b3 = Duration(
+        milliseconds: _completeDuration.inMilliseconds -
+            2 * _bounceDuration.inMilliseconds,
+      );
+      print('I should wait $b3');
+
+      Future<void>.delayed(b3).then(_updateAnimation);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: _indicatorSize + 2 * _indicatorBounceOffset,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) => Positioned(
+                top: _controller.value
+                    .let(Curves.bounceInOut.transform)
+                    .let(_transformIndicator),
+                child: child!,
+              ),
+              child: Container(
+                height: _indicatorSize,
+                width: _indicatorSize,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: widget.color,
+                ),
+                child: widget.icon,
+              ),
+            ),
+          ],
+        ),
+      );
+}
+
+class _Border extends StatelessWidget {
+  const _Border({
+    Key? key,
+    required this.child,
+    required this.isFirst,
+    required this.backgroundColor,
+    required this.isLast,
+  }) : super(key: key);
+
+  final Widget child;
+  final bool isFirst;
+  final Color backgroundColor;
+  final bool isLast;
 
   @override
   Widget build(BuildContext context) => Container(
@@ -161,23 +315,10 @@ class _Indicator extends StatelessWidget {
               : null,
         ),
         padding: (isFirst || isLast)
-            ? EdgeInsets.only(
-                top: isFirst ? 16 : 4,
-                bottom: isLast ? 16 : 4,
-              )
+            ? EdgeInsets.only(top: isFirst ? 16 : 4, bottom: isLast ? 16 : 4)
             : const EdgeInsets.symmetric(vertical: 4),
         margin: EdgeInsets.zero,
-        child: Center(
-          child: Container(
-            width: _indicatorSize,
-            height: _indicatorSize,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: color,
-            ),
-            child: icon,
-          ),
-        ),
+        child: child,
       );
 }
 
@@ -214,6 +355,14 @@ extension on CpTimelineStatus {
   }
 }
 
+double _transformConnector(double value) =>
+    sin(2 * pi * value) / 2 * _connectorHeight;
+
+double _transformIndicator(double value) =>
+    _indicatorBounceOffset + value * _indicatorBounceOffset;
+
+enum _BounceDirection { none, up, down }
+
 const _titleStyle = TextStyle(
   fontWeight: FontWeight.w500,
   fontSize: 16,
@@ -227,5 +376,8 @@ const _subtitleStyle = TextStyle(
 const _timelineRadius = Radius.circular(32);
 const _timelineWidth = 65.0;
 const _connectorHeight = 57.0;
-const _connectorWidth = 7.0;
+const _connectorWidth = 9.0;
 const _indicatorSize = 30.0;
+const _indicatorBounceOffset = _indicatorSize * 0.1;
+const _completeDuration = Duration(seconds: 1);
+const _bounceDuration = Duration(milliseconds: 200);
