a81b6d6cb9016dfbbd64ec0b98e35fc9151fdef4
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-02-19 03:19:48 -0300

    add loader

--- packages/espressocash_app/lib/features/incoming_split_key_payments/src/widgets/extensions.dart
+++ packages/espressocash_app/lib/features/incoming_split_key_payments/src/widgets/extensions.dart
@@ -4,23 +4,26 @@ import 'package:solana/solana.dart';
 
 import '../../../../core/accounts/bl/account.dart';
 import '../../../../di.dart';
+import '../../../../ui/loader.dart';
 import '../bl/incoming_split_key_payment.dart';
 import '../bl/iskp_service.dart';
 
 extension ISKPExt on BuildContext {
-  Future<String> createISKP(Ed25519HDKeyPair escrow) async {
-    final payment = await sl<ISKPService>().create(
-      account: read<MyAccount>().wallet,
-      escrow: escrow,
-    );
+  Future<String> createISKP(Ed25519HDKeyPair escrow) async =>
+      runWithLoader(this, () async {
+        final payment = await sl<ISKPService>().create(
+          account: read<MyAccount>().wallet,
+          escrow: escrow,
+        );
 
-    return payment.id;
-  }
+        return payment.id;
+      });
 
-  Future<void> retryISKP(IncomingSplitKeyPayment payment) async {
-    await sl<ISKPService>().retry(
-      payment,
-      account: read<MyAccount>().wallet,
-    );
-  }
+  Future<void> retryISKP(IncomingSplitKeyPayment payment) async =>
+      runWithLoader(this, () async {
+        await sl<ISKPService>().retry(
+          payment,
+          account: read<MyAccount>().wallet,
+        );
+      });
 }
