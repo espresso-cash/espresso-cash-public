ae08ceffe706cafc4b0653b96a2eb9f591aecf00
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-02-20 17:47:42 -0300

    upd

--- packages/espressocash_app/lib/ui/dialogs.dart
+++ packages/espressocash_app/lib/ui/dialogs.dart
@@ -45,7 +45,14 @@ Future<void> showConfirmationDialog(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(title.toUpperCase(), style: _titleStyle),
+              Text(
+                title.toUpperCase(),
+                style: const TextStyle(
+                  fontSize: 17,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               const SizedBox(height: 24),
               Text(message, style: _messageStyle, textAlign: TextAlign.center),
               const SizedBox(height: 32),
@@ -76,18 +83,49 @@ Future<void> showWarningDialog(
 }) =>
     showDialog(
       context: context,
-      builder: (context) => Theme(
-        data: ThemeData.light(),
-        child: AlertDialog(
-          title: Text(title),
-          content: Text(message, style: _contentStyle),
-          actions: <Widget>[
-            TextButton(
-              style: _confirmButtonStyle,
-              child: Text(context.l10n.ok),
-              onPressed: () => Navigator.pop(context),
+      builder: (context) => CpTheme.dark(
+        child: Dialog(
+          backgroundColor: CpColors.primaryColor,
+          insetPadding: const EdgeInsets.symmetric(horizontal: 24),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(44),
+              bottomLeft: Radius.circular(44),
+              bottomRight: Radius.circular(44),
             ),
-          ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 48),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    height: 1.3,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  message,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                CpButton(
+                  text: context.l10n.ok,
+                  width: 170,
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -100,12 +138,6 @@ const _contentStyle = TextStyle(
 
 final _confirmButtonStyle = TextButton.styleFrom(
   foregroundColor: CpColors.primaryColor,
-);
-
-const _titleStyle = TextStyle(
-  fontSize: 17,
-  color: Colors.white,
-  fontWeight: FontWeight.bold,
 );
 
 const _messageStyle = TextStyle(
