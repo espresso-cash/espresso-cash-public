0ce29c635fabb96463f687a4f6f40fe91d556c81
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-03-02 02:13:22 -0300

    upd tests

--- packages/espressocash_app/test/core/balances/accounts/accounts_bloc_test.dart
+++ packages/espressocash_app/test/core/balances/accounts/accounts_bloc_test.dart
@@ -2,7 +2,6 @@ import 'package:bip39/bip39.dart' as bip39;
 import 'package:bloc_test/bloc_test.dart';
 import 'package:espressocash_app/core/accounts/bl/account.dart';
 import 'package:espressocash_app/core/accounts/bl/accounts_bloc.dart';
-import 'package:espressocash_app/core/accounts/bl/ec_wallet.dart';
 import 'package:espressocash_app/core/accounts/bl/mnemonic.dart';
 import 'package:espressocash_app/core/file_manager.dart';
 import 'package:espressocash_app/core/wallet.dart';
@@ -16,173 +15,159 @@ import 'accounts_bloc_test.mocks.dart';
 
 @GenerateMocks([FlutterSecureStorage, SeedVault])
 Future<void> main() async {
-  group(
-    'Local wallet tests',
-    () {
-      late MockSeedVault seedVault;
-      late MockFlutterSecureStorage storage;
-      late ECWallet wallet;
-      late String mnemonic;
-      late MyAccount testAccount;
-      late FileManager fileManager;
+  final seedVault = MockSeedVault();
+  final storage = MockFlutterSecureStorage();
+  final mnemonic = bip39.generateMnemonic();
+  final wallet = await createLocalWallet(mnemonic: mnemonic);
+  final testAccount = MyAccount(
+    wallet: wallet,
+    firstName: 'Test',
+    accessMode: const AccessMode.loaded(),
+  );
+  final fileManager = FileManager();
 
-      setUp(() async {
-        seedVault = MockSeedVault();
-        storage = MockFlutterSecureStorage();
-        mnemonic = bip39.generateMnemonic();
-        wallet = await createLocalWallet(mnemonic: mnemonic);
-        testAccount = MyAccount(
-          wallet: wallet,
-          firstName: 'Test',
-          accessMode: const AccessMode.loaded(),
-        );
-        fileManager = FileManager();
-      });
+  tearDown(() {
+    reset(storage);
+  });
 
-      tearDown(() {
-        reset(storage);
-      });
-
-      AccountsBloc createBloc() => AccountsBloc(
-            storage: storage,
-            fileManager: fileManager,
-            seedVault: seedVault,
-          );
-
-      blocTest<AccountsBloc, AccountsState>(
-        'Tries to load account from storage',
-        setUp: () {
-          when(
-            storage.read(
-              key: anyNamed('key'),
-              iOptions: anyNamed('iOptions'),
-            ),
-          ).thenAnswer((_) async => null);
-          when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
-              .thenAnswer((_) async {});
-        },
-        build: createBloc,
-        act: (b) {
-          b.add(const AccountsEvent.initialize());
-        },
-        expect: () => [
-          const AccountsState(isProcessing: true),
-          const AccountsState(),
-        ],
-        verify: (_) {
-          verify(
-            storage.read(
-              key: anyNamed('key'),
-              iOptions: anyNamed('iOptions'),
-            ),
-          ).called(3);
-          verifyNever(
-            storage.write(key: anyNamed('key'), value: anyNamed('value')),
-          );
-        },
+  AccountsBloc createBloc() => AccountsBloc(
+        storage: storage,
+        fileManager: fileManager,
+        seedVault: seedVault,
       );
 
-      blocTest<AccountsBloc, AccountsState>(
-        'Loads account from storage if it exists',
-        setUp: () {
-          when(
-            storage.read(key: authTokenKey, iOptions: anyNamed('iOptions')),
-          ).thenAnswer((_) async => null);
-          when(
-            storage.read(key: mnemonicKey, iOptions: anyNamed('iOptions')),
-          ).thenAnswer((_) async => mnemonic);
-          when(
-            storage.read(key: nameKey, iOptions: anyNamed('iOptions')),
-          ).thenAnswer((_) async => testAccount.firstName);
-          when(
-            storage.read(key: photoKey, iOptions: anyNamed('iOptions')),
-          ).thenAnswer((_) async => null);
-          when(
-            storage.read(key: onboardingKey, iOptions: anyNamed('iOptions')),
-          ).thenAnswer((_) async => 'true');
-          when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
-              .thenAnswer((_) async {});
-        },
-        build: createBloc,
-        act: (b) {
-          b.add(const AccountsEvent.initialize());
-        },
-        expect: () => [
-          const AccountsState(isProcessing: true),
-          allOf(
-            predicate(
-              (AccountsState a) => a.account?.address == testAccount.address,
-              'has correct account address',
-            ),
-            predicate(
-              (AccountsState a) => !a.isProcessing,
-              'is not processing',
-            ),
-            predicate(
-              (AccountsState a) => a.hasFinishedOnboarding,
-              'has finished onboarding',
-            ),
-          ),
-        ],
+  blocTest<AccountsBloc, AccountsState>(
+    'Tries to load account from storage',
+    setUp: () {
+      when(
+        storage.read(
+          key: anyNamed('key'),
+          iOptions: anyNamed('iOptions'),
+        ),
+      ).thenAnswer((_) async => null);
+      when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
+          .thenAnswer((_) async {});
+    },
+    build: createBloc,
+    act: (b) {
+      b.add(const AccountsEvent.initialize());
+    },
+    expect: () => [
+      const AccountsState(isProcessing: true),
+      const AccountsState(),
+    ],
+    verify: (_) {
+      verify(
+        storage.read(
+          key: anyNamed('key'),
+          iOptions: anyNamed('iOptions'),
+        ),
+      ).called(3);
+      verifyNever(
+        storage.write(key: anyNamed('key'), value: anyNamed('value')),
       );
+    },
+  );
 
-      blocTest<AccountsBloc, AccountsState>(
-        'Saves account when created',
-        setUp: () {
-          when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
-              .thenAnswer((_) async {});
-        },
-        build: createBloc,
-        act: (b) {
-          b.add(
-            AccountsEvent.created(
-              source: AccountSource.local(Mnemonic.generated(mnemonic)),
-              account: testAccount,
-              hasFinishedOnboarding: false,
-            ),
-          );
-        },
-        expect: () => [
-          const AccountsState(isProcessing: true),
-          allOf(
-            predicate(
-              (AccountsState a) => a.account?.address == testAccount.address,
-              'has correct account address',
-            ),
-            predicate(
-              (AccountsState a) => !a.isProcessing,
-              'is not processing',
-            ),
-            predicate(
-              (AccountsState a) => !a.hasFinishedOnboarding,
-              'has not finished onboarding',
-            ),
-          ),
-        ],
-        verify: (_) {
-          verify(
-            storage.write(
-              key: mnemonicKey,
-              value: mnemonic,
-              iOptions: anyNamed('iOptions'),
-            ),
-          ).called(1);
-          verify(
-            storage.write(
-              key: nameKey,
-              value: testAccount.firstName,
-              iOptions: anyNamed('iOptions'),
-            ),
-          ).called(1);
-          verify(
-            storage.write(
-              key: onboardingKey,
-              value: 'false',
-              iOptions: anyNamed('iOptions'),
-            ),
-          ).called(1);
-        },
+  blocTest<AccountsBloc, AccountsState>(
+    'Loads account from storage if it exists',
+    setUp: () {
+      when(
+        storage.read(key: authTokenKey, iOptions: anyNamed('iOptions')),
+      ).thenAnswer((_) async => null);
+      when(
+        storage.read(key: mnemonicKey, iOptions: anyNamed('iOptions')),
+      ).thenAnswer((_) async => mnemonic);
+      when(
+        storage.read(key: nameKey, iOptions: anyNamed('iOptions')),
+      ).thenAnswer((_) async => testAccount.firstName);
+      when(
+        storage.read(key: photoKey, iOptions: anyNamed('iOptions')),
+      ).thenAnswer((_) async => null);
+      when(
+        storage.read(key: onboardingKey, iOptions: anyNamed('iOptions')),
+      ).thenAnswer((_) async => 'true');
+      when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
+          .thenAnswer((_) async {});
+    },
+    build: createBloc,
+    act: (b) {
+      b.add(const AccountsEvent.initialize());
+    },
+    expect: () => [
+      const AccountsState(isProcessing: true),
+      allOf(
+        predicate(
+          (AccountsState a) => a.account?.address == testAccount.address,
+          'has correct account address',
+        ),
+        predicate(
+          (AccountsState a) => !a.isProcessing,
+          'is not processing',
+        ),
+        predicate(
+          (AccountsState a) => a.hasFinishedOnboarding,
+          'has finished onboarding',
+        ),
+      ),
+    ],
+  );
+
+  blocTest<AccountsBloc, AccountsState>(
+    'Saves account when created',
+    setUp: () {
+      when(storage.write(key: anyNamed('key'), value: anyNamed('value')))
+          .thenAnswer((_) async {});
+    },
+    build: createBloc,
+    act: (b) {
+      b.add(
+        AccountsEvent.created(
+          source: AccountSource.local(Mnemonic.generated(mnemonic)),
+          account: testAccount,
+          hasFinishedOnboarding: false,
+        ),
       );
+    },
+    expect: () => [
+      const AccountsState(isProcessing: true),
+      allOf(
+        predicate(
+          (AccountsState a) => a.account?.address == testAccount.address,
+          'has correct account address',
+        ),
+        predicate(
+          (AccountsState a) => !a.isProcessing,
+          'is not processing',
+        ),
+        predicate(
+          (AccountsState a) => !a.hasFinishedOnboarding,
+          'has not finished onboarding',
+        ),
+      ),
+    ],
+    verify: (_) {
+      verify(
+        storage.write(
+          key: mnemonicKey,
+          value: mnemonic,
+          iOptions: anyNamed('iOptions'),
+        ),
+      ).called(1);
+      verify(
+        storage.write(
+          key: nameKey,
+          value: testAccount.firstName,
+          iOptions: anyNamed('iOptions'),
+        ),
+      ).called(1);
+      verify(
+        storage.write(
+          key: onboardingKey,
+          value: 'false',
+          iOptions: anyNamed('iOptions'),
+        ),
+      ).called(1);
     },
   );
 }
