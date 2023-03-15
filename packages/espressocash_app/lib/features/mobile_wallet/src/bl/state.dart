7b33d72f6fb8da2a0764b3d27d9b2dd2e867ad13
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-03-15 20:08:02 -0300

    upd

--- packages/espressocash_app/lib/features/mobile_wallet/src/bl/state.dart
+++ packages/espressocash_app/lib/features/mobile_wallet/src/bl/state.dart
@@ -4,9 +4,8 @@ part of 'bloc.dart';
 class RemoteRequestState with _$RemoteRequestState {
   const factory RemoteRequestState.requested(RemoteRequest request) =
       _Requested;
-      
-  const factory RemoteRequestState.loading() =
-      _Loading;
+
+  const factory RemoteRequestState.loading() = _Loading;
 
   const factory RemoteRequestState.result(Object? result) = _Result;
 }
