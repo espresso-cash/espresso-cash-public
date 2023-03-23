e29a8c71f56cd171096a0c47b55d09681857cbd8
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-03-23 13:57:27 -0300

    feat: add dynamic texts to onboarding notice

--- packages/espressocash_app/lib/features/onboarding/widgets/onboarding_notice.dart
+++ packages/espressocash_app/lib/features/onboarding/widgets/onboarding_notice.dart
@@ -4,7 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/accounts/bl/accounts_bloc.dart';
 import '../../../gen/assets.gen.dart';
+import '../../../l10n/l10n.dart';
 import '../../../routes.gr.dart';
+import '../../../ui/button.dart';
 
 class OnboardingNotice extends StatelessWidget {
   const OnboardingNotice({super.key});
@@ -12,20 +14,100 @@ class OnboardingNotice extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       BlocBuilder<AccountsBloc, AccountsState>(
-        builder: (context, state) => state.hasFinishedOnboarding
-            ? const SizedBox.shrink()
-            : SizedBox(
-                height: 125,
-                child: RepaintBoundary(
-                  child: GestureDetector(
-                    onTap: () =>
-                        context.router.navigate(const OnboardingFlowRoute()),
-                    child: Assets.rive.onboardingNotice.rive(
+        builder: (context, state) {
+          if (!state.hasFinishedOnboarding) return const SizedBox.shrink();
+
+          void onPressed() =>
+              context.router.navigate(const OnboardingFlowRoute());
+
+          return AspectRatio(
+            aspectRatio: 433 / 123,
+            child: GestureDetector(
+              onTap: onPressed,
+              child: RepaintBoundary(
+                child: Stack(
+                  children: [
+                    Assets.rive.onboardingNotice.rive(
                       alignment: Alignment.topCenter,
                       fit: BoxFit.fitWidth,
                     ),
-                  ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: _Content(
+                        onPressed: onPressed,
+                      ),
+                    ),
+                  ],
                 ),
               ),
+            ),
+          );
+        },
+      );
+}
+
+class _Content extends StatelessWidget {
+  const _Content({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.fromLTRB(24, 16, 24, 8),
+        child: FractionallySizedBox(
+          widthFactor: 0.75,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Flexible(
+                child: _Text(),
+              ),
+              const SizedBox(height: 8),
+              Flexible(
+                child: CpButton(
+                  text: context.l10n.onboardingNoticeFinishSetup,
+                  size: CpButtonSize.micro,
+                  onPressed: onPressed,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class _Text extends StatelessWidget {
+  const _Text({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: FittedBox(
+              child: Text(
+                context.l10n.onboardingNoticeTitle,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          FittedBox(
+            child: Text(
+              context.l10n.onboardingNoticeMessage,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
       );
 }
