2839120e4abbd9e73fc741f6329b05def38d9681
Justin Enerio <justinenerio@pm.me>
2023-03-27 14:52:53 +0800

    fix analyzer

--- packages/espressocash_app/lib/features/token_chart/src/data/coingecko_client.dart
+++ packages/espressocash_app/lib/features/token_chart/src/data/coingecko_client.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:dio/dio.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
