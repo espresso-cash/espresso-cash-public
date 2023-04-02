8756e8e4374b7430d5c780eb39287fddfdd8e996
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-04-01 23:17:01 -0300

    upd

---
+++ packages/espressocash_app/lib/features/token_details/src/widgets/token_appbar.dart
@@ -0,0 +1,108 @@
+import 'dart:math';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../../../../core/tokens/token.dart';
+import '../../../../ui/colors.dart';
+import '../../../../ui/token_icon.dart';
+import '../../../favorite_tokens/widgets/favorite_button.dart';
+
+class TokenAppBarDelegate extends SliverPersistentHeaderDelegate {
+  const TokenAppBarDelegate({
+    required this.token,
+  });
+
+  final Token token;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final ratio = shrinkOffset / maxExtent;
+    final iconSize = max(_tokenSize * (1 - ratio), 24.0);
+
+    return Material(
+      color: CpColors.darkBackground,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Stack(
+          children: [
+            _buildIcon(ratio, iconSize),
+            _buildText(ratio, iconSize),
+            Positioned(
+              top: 0,
+              child: _Buttons(token: token),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(double ratio, double iconSize) => Positioned(
+        top: (iconSize * (1 - ratio)) - iconSize,
+        left: 0,
+        right: 0,
+        child: Opacity(
+          opacity: 1 - ratio,
+          child: CpTokenIcon(token: token, size: iconSize),
+        ),
+      );
+
+  Widget _buildText(double ratio, double iconSize) => Positioned.fill(
+        top: iconSize * (1 - ratio),
+        child: Center(
+          child: Text(
+            token.name,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 26,
+            ),
+          ),
+        ),
+      );
+
+  @override
+  double get maxExtent => _tokenSize + _minExtent;
+
+  @override
+  double get minExtent => _minExtent;
+
+  @override
+  bool shouldRebuild(covariant TokenAppBarDelegate oldDelegate) =>
+      oldDelegate.token != token;
+
+  @override
+  FloatingHeaderSnapConfiguration? get snapConfiguration =>
+      FloatingHeaderSnapConfiguration(
+        curve: Curves.easeIn,
+        duration: const Duration(milliseconds: 200),
+      );
+}
+
+class _Buttons extends StatelessWidget {
+  const _Buttons({Key? key, required this.token}) : super(key: key);
+
+  final Token token;
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: _minExtent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BackButton(onPressed: () => context.router.pop()),
+            FavoriteButton(token: token),
+          ],
+        ),
+      );
+}
+
+const double _tokenSize = 68;
+const double _minExtent = kToolbarHeight;
