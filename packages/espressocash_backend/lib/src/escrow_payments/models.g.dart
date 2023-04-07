7ebc57553aecf7f90a452c82f25cf23d0d8c36f0
ookami-kb <ookami.kb@gmail.com>
2023-04-07 23:40:34 +0200

    upd

--- packages/espressocash_backend/lib/src/escrow_payments/models.g.dart
+++ packages/espressocash_backend/lib/src/escrow_payments/models.g.dart
@@ -6,68 +6,6 @@ part of 'models.dart';
 // BorshSerializableGenerator
 // **************************************************************************
 
-mixin _$_EscrowDataAccount {
-  Ed25519HDPublicKey get senderAccount => throw UnimplementedError();
-  Ed25519HDPublicKey get vaultTokenAccount => throw UnimplementedError();
-  Ed25519HDPublicKey get senderTokenAccount => throw UnimplementedError();
-  Ed25519HDPublicKey get depositorAccount => throw UnimplementedError();
-  int get status => throw UnimplementedError();
-
-  Uint8List toBorsh() {
-    final writer = BinaryWriter();
-
-    const BPublicKey().write(writer, senderAccount);
-    const BPublicKey().write(writer, vaultTokenAccount);
-    const BPublicKey().write(writer, senderTokenAccount);
-    const BPublicKey().write(writer, depositorAccount);
-    const BU8().write(writer, status);
-
-    return writer.toArray();
-  }
-}
-
-class __EscrowDataAccount extends _EscrowDataAccount {
-  __EscrowDataAccount({
-    required this.senderAccount,
-    required this.vaultTokenAccount,
-    required this.senderTokenAccount,
-    required this.depositorAccount,
-    required this.status,
-  }) : super._();
-
-  final Ed25519HDPublicKey senderAccount;
-  final Ed25519HDPublicKey vaultTokenAccount;
-  final Ed25519HDPublicKey senderTokenAccount;
-  final Ed25519HDPublicKey depositorAccount;
-  final int status;
-}
-
-class B_EscrowDataAccount implements BType<_EscrowDataAccount> {
-  const B_EscrowDataAccount();
-
-  @override
-  void write(BinaryWriter writer, _EscrowDataAccount value) {
-    writer.writeStruct(value.toBorsh());
-  }
-
-  @override
-  _EscrowDataAccount read(BinaryReader reader) {
-    return _EscrowDataAccount(
-      senderAccount: const BPublicKey().read(reader),
-      vaultTokenAccount: const BPublicKey().read(reader),
-      senderTokenAccount: const BPublicKey().read(reader),
-      depositorAccount: const BPublicKey().read(reader),
-      status: const BU8().read(reader),
-    );
-  }
-}
-
-_EscrowDataAccount _$_EscrowDataAccountFromBorsh(Uint8List data) {
-  final reader = BinaryReader(data.buffer.asByteData());
-
-  return const B_EscrowDataAccount().read(reader);
-}
-
 mixin _$EscrowArgument {
   BigInt get amount => throw UnimplementedError();
 
