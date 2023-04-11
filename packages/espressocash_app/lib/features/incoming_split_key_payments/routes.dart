ed78f673a6c16131bc12db4c68866fab0069c0aa
Justin Enerio <justinenerio@pm.me>
2023-04-11 16:34:05 +0800

    upd rename files

--- packages/espressocash_app/lib/features/incoming_split_key_payments/routes.dart
+++ packages/espressocash_app/lib/features/incoming_split_key_payments/routes.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 
-import 'src/widgets/first_part_qr.dart';
+import 'src/widgets/first_part_qr_screen.dart';
 import 'src/widgets/first_part_ready_screen.dart';
 import 'src/widgets/incoming_split_key_payment_screen.dart';
 
