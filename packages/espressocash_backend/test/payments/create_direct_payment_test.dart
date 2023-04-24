0638407970aa03e45258112233dbadfa8f2d009d
Justin Enerio <justinenerio@pm.me>
2023-04-24 19:20:31 +0800

    revert

--- packages/espressocash_backend/test/payments/create_direct_payment_test.dart
+++ packages/espressocash_backend/test/payments/create_direct_payment_test.dart
@@ -1,12 +1,13 @@
 import 'package:espressocash_backend/src/constants.dart';
-import 'package:espressocash_backend/src/payments/create_payment.dart';
+import 'package:espressocash_backend/src/payments/create_direct_payment.dart';
 import 'package:solana/solana.dart';
-import 'package:test/test.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 import '../config.dart';
 import 'utils.dart';
 
-Future<void> main() async {
+void main() {
   final client = createTestSolanaClient();
 
   /// Initial token amount for the sender after creation.
@@ -14,11 +15,7 @@ Future<void> main() async {
 
   const transferAmount = 300000;
 
-  /// Creates sender system account and ATA, minting [senderInitialAmount] to
-  /// the ATA. System account won't have any SOL, since sender is not paying any
-  /// fees in SOL.
-
-  test('creates payment', () async {
+  test('Receiver does not have ATA', () async {
     final data = await createTestData(
       client: client,
       senderInitialAmount: senderInitialAmount,
@@ -27,23 +24,24 @@ Future<void> main() async {
     final platform = data.platform;
     final mint = data.mint;
 
-    // Sender creates an escrow account and keeps its private key. Sender
-    // doesn't transfer private key for the escrow account to the system.
-    final escrow = await Ed25519HDKeyPair.random();
+    final receiver = await Ed25519HDKeyPair.random();
 
-    final result = await createPaymentTx(
+    final payment = await createDirectPayment(
       aSender: sender.publicKey,
-      aEscrow: escrow.publicKey,
+      aReceiver: receiver.publicKey,
+      aReference: null,
       mint: mint,
       amount: transferAmount,
-      client: client,
       platform: platform,
+      client: client,
       commitment: Commitment.confirmed,
     );
 
+    expect(payment.fee, directPaymentWithAccountCreationFee);
+
     // Sender has to resign the transaction with their private key. The tx is
     // already partially signed by the platform.
-    final resignedTx = await sender.resign(result.item1);
+    final resignedTx = await sender.resign(payment.transaction);
 
     final signature = await client.rpcClient.sendTransaction(
       resignedTx.encode(),
@@ -57,19 +55,19 @@ Future<void> main() async {
 
     expect(
       await client.getMintBalance(sender.publicKey, mint: mint),
-      senderInitialAmount - transferAmount - shareableLinkPaymentFee,
+      senderInitialAmount - transferAmount - payment.fee,
     );
     expect(
-      await client.getMintBalance(escrow.publicKey, mint: mint),
+      await client.getMintBalance(receiver.publicKey, mint: mint),
       transferAmount,
     );
     expect(
       await client.getMintBalance(platform.publicKey, mint: mint),
-      shareableLinkPaymentFee,
+      payment.fee,
     );
   });
 
-  test('fails without resigning', () async {
+  test('Receiver has ATA', () async {
     final data = await createTestData(
       client: client,
       senderInitialAmount: senderInitialAmount,
@@ -78,23 +76,53 @@ Future<void> main() async {
     final platform = data.platform;
     final mint = data.mint;
 
-    final escrow = await Ed25519HDKeyPair.random();
+    final receiver = await Ed25519HDKeyPair.random();
 
-    final result = await createPaymentTx(
-      aSender: sender.publicKey,
-      aEscrow: escrow.publicKey,
+    await client.createAssociatedTokenAccount(
       mint: mint,
-      amount: transferAmount,
-      client: client,
-      platform: platform,
+      funder: data.mintAuthority,
+      owner: receiver.publicKey,
       commitment: Commitment.confirmed,
     );
 
-    final signature = client.rpcClient.sendTransaction(
-      result.item1.encode(),
+    final payment = await createDirectPayment(
+      aSender: sender.publicKey,
+      aReceiver: receiver.publicKey,
+      aReference: null,
+      mint: mint,
+      amount: transferAmount,
+      platform: platform,
+      client: client,
+      commitment: Commitment.confirmed,
+    );
+
+    expect(payment.fee, directPaymentFee);
+
+    // Sender has to resign the transaction with their private key. The tx is
+    // already partially signed by the platform.
+    final resignedTx = await sender.resign(payment.transaction);
+
+    final signature = await client.rpcClient.sendTransaction(
+      resignedTx.encode(),
       preflightCommitment: Commitment.confirmed,
     );
 
-    expect(signature, throwsA(isA<JsonRpcException>()));
+    await client.waitForSignatureStatus(
+      signature,
+      status: Commitment.confirmed,
+    );
+
+    expect(
+      await client.getMintBalance(sender.publicKey, mint: mint),
+      senderInitialAmount - transferAmount - payment.fee,
+    );
+    expect(
+      await client.getMintBalance(receiver.publicKey, mint: mint),
+      transferAmount,
+    );
+    expect(
+      await client.getMintBalance(platform.publicKey, mint: mint),
+      payment.fee,
+    );
   });
 }
