7fb1598293d665abfb624cef90f636cafcadc28c
Justin Enerio <justinenerio@pm.me>
2023-06-09 13:59:28 +1000

    upd

--- packages/espressocash_app/lib/features/sign_in/src/widgets/create_wallet_screen.dart
+++ packages/espressocash_app/lib/features/sign_in/src/widgets/create_wallet_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solana_seed_vault/solana_seed_vault.dart';
@@ -6,6 +7,7 @@ import '../../../../di.dart';
 import '../../../../ui/splash_screen.dart';
 import '../bl/sign_in_bloc.dart';
 
+@RoutePage()
 class CreateWalletLoadingScreen extends StatefulWidget {
   const CreateWalletLoadingScreen({super.key});
 
