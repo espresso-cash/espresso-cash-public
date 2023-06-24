535b3ccb51f2348edb81c4fd93b79209b2f12f80
ookami-kb <ookami.kb@gmail.com>
2023-06-24 11:46:19 +0200

    refactor: add AccountRepository

--- packages/espressocash_app/lib/features/accounts/services/accounts_bloc.dart
+++ packages/espressocash_app/lib/features/accounts/services/accounts_bloc.dart
@@ -1,14 +1,12 @@
 import 'package:bloc_concurrency/bloc_concurrency.dart';
-import 'package:dfunc/dfunc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:solana_seed_vault/solana_seed_vault.dart';
 
-import '../../../core/wallet.dart';
+import '../data/account_repository.dart';
 import '../models/account.dart';
-import '../models/ec_wallet.dart';
 import '../models/mnemonic.dart';
 
 part 'accounts_bloc.freezed.dart';
@@ -20,14 +18,17 @@ class AccountsBloc extends Bloc<AccountsEvent, AccountsState> {
   AccountsBloc({
     required FlutterSecureStorage storage,
     required SeedVault seedVault,
+    required AccountRepository repository,
   })  : _storage = storage,
         _seedVault = seedVault,
+        _repository = repository,
         super(const AccountsState(isProcessing: true)) {
     on<AccountsEvent>(_eventHandler, transformer: sequential());
   }
 
   final FlutterSecureStorage _storage;
   final SeedVault _seedVault;
+  final AccountRepository _repository;
 
   EventHandler<AccountsEvent, AccountsState> get _eventHandler =>
       (event, emit) => event.map(
@@ -39,7 +40,7 @@ class AccountsBloc extends Bloc<AccountsEvent, AccountsState> {
   Future<void> _onInitialize(Emitter<AccountsState> emit) async {
     emit(state.copyWith(isProcessing: true));
     try {
-      final account = await _storage.loadAccount(_seedVault);
+      final account = await _repository.loadAccount(_seedVault);
 
       emit(state.copyWith(account: account, isProcessing: false));
     } on Exception {
@@ -50,14 +51,14 @@ class AccountsBloc extends Bloc<AccountsEvent, AccountsState> {
   Future<void> _onCreated(Created event, Emitter<AccountsState> emit) async {
     emit(state.copyWith(isProcessing: true));
 
-    await _storage.saveAccountSource(event.source);
+    await _repository.saveAccountSource(event.source);
 
     emit(state.copyWith(account: event.account, isProcessing: false));
   }
 
   Future<void> _onLoggedOut(Emitter<AccountsState> emit) async {
     try {
-      final authToken = await loadAuthToken(_storage);
+      final authToken = await _repository.loadAuthToken();
       if (authToken != null) {
         await _seedVault.deauthorizeSeed(authToken);
       }
@@ -67,45 +68,3 @@ class AccountsBloc extends Bloc<AccountsEvent, AccountsState> {
     }
   }
 }
-
-Future<String> loadMnemonic(FlutterSecureStorage storage) =>
-    storage.read(key: mnemonicKey).letAsync((it) => it ?? '');
-
-Future<AuthToken?> loadAuthToken(FlutterSecureStorage storage) => storage
-    .read(key: authTokenKey)
-    .letAsync((it) => it.toString())
-    .letAsync(AuthToken.tryParse);
-
-extension on FlutterSecureStorage {
-  Future<void> saveAccountSource(AccountSource source) => source.when(
-        local: (it) => write(key: mnemonicKey, value: it.phrase),
-        saga: (it) => write(key: authTokenKey, value: it.toString()),
-      );
-
-  /// Loads existing account if wallet data exists in [FlutterSecureStorage].
-  Future<MyAccount?> loadAccount(SeedVault seedVault) async {
-    final mnemonic = await loadMnemonic(this);
-    final authToken = await loadAuthToken(this);
-
-    final ECWallet wallet;
-
-    if (authToken != null) {
-      wallet = await createSagaWallet(seedVault, authToken);
-    } else if (mnemonic.isNotEmpty) {
-      wallet = await createLocalWallet(mnemonic: mnemonic);
-    } else {
-      return null;
-    }
-
-    return MyAccount(
-      accessMode: const AccessMode.loaded(),
-      wallet: wallet,
-    );
-  }
-}
-
-@visibleForTesting
-const authTokenKey = 'authToken';
-
-@visibleForTesting
-const mnemonicKey = 'mnemonic';
--- packages/espressocash_app/lib/features/onboarding/screens/create_recovery_phrase_screen.dart
+++ packages/espressocash_app/lib/features/onboarding/screens/create_recovery_phrase_screen.dart
@@ -1,13 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../di.dart';
 import '../../../l10n/l10n.dart';
 import '../../../ui/app_bar.dart';
 import '../../../ui/onboarding_screen.dart';
 import '../../../ui/recovery_phrase_text_view.dart';
 import '../../../ui/theme.dart';
-import '../../backup_phrase/data/mnemonic_getter.dart';
+import '../../accounts/data/account_repository.dart';
 
 @RoutePage()
 class ViewRecoveryPhraseScreen extends StatefulWidget {
@@ -26,7 +26,7 @@ class _ViewRecoveryPhraseScreenState extends State<ViewRecoveryPhraseScreen> {
   @override
   void initState() {
     super.initState();
-    context.read<MnemonicGetter>().mnemonic.then((String? phrase) {
+    sl<AccountRepository>().loadMnemonic().then((String? phrase) {
       if (phrase != null) {
         setState(() => _phrase = phrase);
       }
