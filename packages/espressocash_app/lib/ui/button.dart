a220e442dfb21481cef82f9aa788afcdcc810ea3
Justin Enerio <justinenerio@pm.me>
2023-10-03 04:04:06 +0800

    feat: add payment landing site (#1069)
    Co-authored-by: ookami-kb <ookami.kb@gmail.com>

--- packages/espressocash_app/lib/ui/button.dart
+++ packages/espressocash_app/lib/ui/button.dart
@@ -25,6 +25,7 @@ class CpButton extends StatelessWidget {
     this.size = CpButtonSize.normal,
     this.alignment = CpButtonAlignment.center,
     this.trailing,
+    this.leading,
   });
 
   final String text;
@@ -35,6 +36,7 @@ class CpButton extends StatelessWidget {
   final CpButtonSize size;
   final CpButtonAlignment alignment;
   final Widget? trailing;
+  final Widget? leading;
 
   Color get _backgroundColor {
     switch (variant) {
@@ -127,14 +129,20 @@ class CpButton extends StatelessWidget {
       ),
       child: SizedBox(
         width: trailing == null ? null : double.infinity,
+        height: size.height,
         child: Stack(
           alignment: Alignment.center,
           children: [
+            if (leading case final leading?)
+              Positioned(
+                left: 0,
+                child: leading,
+              ),
             Text(text),
-            if (trailing case final leading?)
+            if (trailing case final trailing?)
               Positioned(
                 right: 0,
-                child: leading,
+                child: trailing,
               ),
           ],
         ),
