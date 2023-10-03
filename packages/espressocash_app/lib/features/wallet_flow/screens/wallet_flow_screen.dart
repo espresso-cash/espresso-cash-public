5e1d2731ebaa4ee98501f56a00b1cb7b4ba7c6f6
Justin Enerio <justinenerio@pm.me>
2023-10-03 10:33:17 +0800

    upd

--- packages/espressocash_app/lib/features/wallet_flow/screens/wallet_flow_screen.dart
+++ packages/espressocash_app/lib/features/wallet_flow/screens/wallet_flow_screen.dart
@@ -11,17 +11,14 @@ import '../../../l10n/l10n.dart';
 import '../../../routes.gr.dart';
 import '../../../ui/shake.dart';
 import '../../conversion_rates/services/amount_ext.dart';
-import '../../outgoing_link_payments/screens/olp_confirmation_screen.dart';
-import '../../outgoing_link_payments/screens/olp_screen.dart';
-import '../../outgoing_link_payments/widgets/extensions.dart';
 import '../../outgoing_direct_payments/data/blockchain.dart';
 import '../../outgoing_direct_payments/screens/odp_confirmation_screen.dart';
 import '../../outgoing_direct_payments/screens/odp_details_screen.dart';
 import '../../outgoing_direct_payments/screens/odp_input_screen.dart';
 import '../../outgoing_direct_payments/widgets/extensions.dart';
-import '../../outgoing_split_key_payments/screens/oskp_confirmation_screen.dart';
-import '../../outgoing_split_key_payments/screens/oskp_screen.dart';
-import '../../outgoing_split_key_payments/widgets/extensions.dart';
+import '../../outgoing_link_payments/screens/olp_confirmation_screen.dart';
+import '../../outgoing_link_payments/screens/olp_screen.dart';
+import '../../outgoing_link_payments/widgets/extensions.dart';
 import '../../payment_request/screens/link_request_flow_screen.dart';
 import '../../qr_scanner/widgets/build_context_ext.dart';
 import 'pay_flow_screen.dart';
@@ -111,13 +108,13 @@ class _State extends State<WalletFlowScreen> {
                 currency: Currency.usdc,
               ),
               onSubmit: () async {
-                final id = await context.createOSKP(amount: cryptoAmount);
+                final id = await context.createOLP(amount: cryptoAmount);
                 if (!mounted) return;
 
                 await context.router.pop();
                 if (!mounted) return;
 
-                await context.router.replace(OSKPScreen.route(id: id));
+                await context.router.replace(OLPScreen.route(id: id));
                 if (!mounted) return;
 
                 setState(() => _fiatAmount = _fiatAmount.copyWith(value: 0));
