139a5dfbc6454a9329f1a7510ae9d27197936735
Justin Enerio <justinenerio@pm.me>
2023-10-05 10:53:15 +0800

    revert landing

--- packages/espressocash_app/lib/landing/main.dart
+++ packages/espressocash_app/lib/landing/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import '../l10n/gen/app_localizations.dart';
 import 'screens/landing.dart';
