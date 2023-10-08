be57d0d3e871a4eb97368e9a6b72a6b54d0dfc46
ookami-kb <ookami.kb@gmail.com>
2023-10-08 22:05:25 +0200

    upd

--- packages/espressocash_app/lib/landing/main.dart
+++ packages/espressocash_app/lib/landing/main.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:html' as html;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
+import '../config.dart';
 import '../l10n/gen/app_localizations.dart';
 import 'screens/landing.dart';
 
@@ -23,14 +28,24 @@ class LandingPageApp extends StatelessWidget {
         theme: ThemeData(fontFamily: 'RobotoApp'),
         color: const Color(0xffE36E0A),
         onGenerateRoute: (settings) {
-          final uri = Uri.parse(settings.name ?? '');
-          final key = uri.queryParameters['k'];
+          final hostname = html.window.location.hostname;
 
-          return uri.path == '/' && key != null
-              ? MaterialPageRoute(
-                  builder: (_) => LandingScreen(encodedKey: key),
-                )
-              : MaterialPageRoute(builder: (_) => const SizedBox.shrink());
+          if (hostname == espressoCashLinkDomain) {
+            final uri = Uri.parse(settings.name ?? '');
+            final key = uri.queryParameters['k'];
+
+            return uri.path == '/' && key != null
+                ? MaterialPageRoute(
+                    builder: (_) => LandingScreen(encodedKey: key),
+                  )
+                : MaterialPageRoute(builder: (_) => const SizedBox.shrink());
+          } else if (hostname == solanaPayHost) {
+            return MaterialPageRoute(
+              builder: (_) => const Placeholder(),
+            );
+          }
+
+          return MaterialPageRoute(builder: (_) => const SizedBox.shrink());
         },
       );
 }
