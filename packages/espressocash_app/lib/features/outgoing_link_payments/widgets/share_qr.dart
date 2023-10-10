94f0fce62a5ebd5f59cb54c39e2aabe215bdfc01
Justin Enerio <justinenerio@pm.me>
2023-10-10 22:16:11 +0800

    feat: update pay link share screen ui

--- packages/espressocash_app/lib/features/outgoing_link_payments/widgets/share_qr.dart
+++ packages/espressocash_app/lib/features/outgoing_link_payments/widgets/share_qr.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../core/amount.dart';
 import '../../../core/presentation/format_amount.dart';
-import '../../../gen/assets.gen.dart';
 import '../../../l10n/l10n.dart';
-import '../../../ui/colors.dart';
-import '../../../ui/content_padding.dart';
 import '../../../ui/rounded_rectangle.dart';
 
 class ShareQr extends StatelessWidget {
@@ -23,130 +20,62 @@ class ShareQr extends StatelessWidget {
   Widget build(BuildContext context) {
     final amount = this.amount.formatWithFiat(context);
 
-    return CpContentPadding(
-      child: CpRoundedRectangle(
-        scrollable: false,
-        backgroundColor: Colors.black,
-        child: Column(
-          children: [
-            const _QrInfoHeader(),
-            const Divider(color: CpColors.darkDividerColor),
-            Expanded(
-              child: _QrBody(
-                amount: amount,
-                qrLink: qrLink.toString(),
-              ),
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+      child: Column(
+        children: [
+          Flexible(
+            child: _QrCodeWrapper(amount: amount, qrData: qrLink.toString()),
+          ),
+        ],
       ),
     );
   }
 }
 
-class _QrInfoHeader extends StatelessWidget {
-  const _QrInfoHeader();
-
-  @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.fromLTRB(24, 28, 24, 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Assets.icons.qrScanner.svg(height: 26),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FittedBox(
-                    child: Text(
-                      context.l10n.qrScanInfo1,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  FittedBox(
-                    child: Text(
-                      context.l10n.qrScanInfo2,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-}
-
-class _QrBody extends StatelessWidget {
-  const _QrBody({
+class _QrCodeWrapper extends StatelessWidget {
+  const _QrCodeWrapper({
     required this.amount,
-    required this.qrLink,
+    required this.qrData,
   });
 
   final String amount;
-  final String qrLink;
+  final String qrData;
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(18),
-        child: LayoutBuilder(
-          builder: (context, constraints) => Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                context.l10n.scanToReceive(amount),
-                maxLines: 1,
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
+  Widget build(BuildContext context) {
+    final amount = this.amount;
+
+    return CpRoundedRectangle(
+      backgroundColor: Colors.black,
+      scrollable: false,
+      margin: const EdgeInsets.symmetric(vertical: 24),
+      padding: const EdgeInsets.all(32),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: Text(
+              context.l10n.scanToReceive(amount),
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 12),
-              Expanded(
-                child: _CustomQrCode(
-                  qrLink: qrLink,
-                  height: constraints.maxWidth,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-}
-
-class _CustomQrCode extends StatelessWidget {
-  const _CustomQrCode({
-    required this.qrLink,
-    required this.height,
-  });
-
-  final String qrLink;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) => LayoutBuilder(
-        builder: (context, constraints) => Stack(
-          alignment: Alignment.center,
-          children: [
-            BarcodeWidget(
-              height: height,
-              barcode: Barcode.qrCode(),
-              data: qrLink,
-              padding: EdgeInsets.zero,
-              color: Colors.white,
             ),
-          ],
-        ),
-      );
+          ),
+          BarcodeWidget(
+            height: 250,
+            barcode: Barcode.qrCode(),
+            data: qrData,
+            padding: EdgeInsets.zero,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }
