0e0a5a24a56187b9465c0b3d93ec065c99cef91d
Justin Enerio <justinenerio@pm.me>
2023-10-18 09:08:55 +0800

    upd video demo

--- packages/espressocash_app/lib/univeral_pay/config.dart
+++ packages/espressocash_app/lib/univeral_pay/config.dart
@@ -4,4 +4,4 @@ const githubRepoUrl = 'https://github.com/espresso-cash/dln-forwarders';
 
 const apiBaseUrl = 'https://app-4kaooywjoa-uc.a.run.app';
 
-const demoVideoUrl = 'https://www.youtube.com/';
+const demoVideoUrl = 'https://www.youtube.com/watch?v=stMbg4HvccA';
