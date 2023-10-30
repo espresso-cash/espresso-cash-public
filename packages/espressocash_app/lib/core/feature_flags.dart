c5f117d1600eb04339e45e7c78a7b031aa320954
Justin Enerio <justinenerio@pm.me>
2023-10-30 23:12:49 +0800

    feat: add experimental flag

--- packages/espressocash_app/lib/core/feature_flags.dart
+++ packages/espressocash_app/lib/core/feature_flags.dart
@@ -1,3 +1,4 @@
+import 'package:dfunc/dfunc.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:injectable/injectable.dart';
 
@@ -18,4 +19,10 @@ class FeatureFlagsManager {
     );
     await _remoteConfig.fetchAndActivate();
   }
+
+  bool isExperimental({required String address}) => _remoteConfig
+      .getString(FeatureFlag.isExperimental.name)
+      .let((p) => p.contains(address));
 }
+
+enum FeatureFlag { isExperimental }
