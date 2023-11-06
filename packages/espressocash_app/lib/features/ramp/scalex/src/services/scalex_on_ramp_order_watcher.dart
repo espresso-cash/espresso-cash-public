1f89c1ba0719d45aaa2c83af22157b21f7d3adfc
Justin Enerio <justinenerio@pm.me>
2023-11-07 00:39:47 +0800

    upd

---
+++ packages/espressocash_app/lib/features/ramp/scalex/src/services/scalex_on_ramp_order_watcher.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:drift/drift.dart';
+import 'package:injectable/injectable.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../../../../../core/currency.dart';
+import '../../../../../data/db/db.dart';
+import '../data/scalex_api_client.dart';
+
+@injectable
+class ScalexOnRampOrderWatcher {
+  ScalexOnRampOrderWatcher(this._db, this._client);
+
+  final MyDatabase _db;
+  final ScalexApiClient _client;
+
+  StreamSubscription<void>? _subscription;
+
+  void watch(String orderId) {
+    _subscription = Stream<void>.periodic(const Duration(seconds: 10))
+        .asyncMap(
+          (_) {
+            final query = _db.select(_db.onRampOrderRows)
+              ..where(
+                (tbl) => tbl.id.equals(orderId) & tbl.isCompleted.equals(false),
+              );
+
+            return query.getSingleOrNull();
+          },
+        )
+        .whereNotNull()
+        .asyncMap((order) => _client.getTransaction(order.partnerOrderId))
+        .listen((event) async {
+          // ignore: prefer-early-return, cannot use
+          if (event.data case final data) {
+            final statement = _db.update(_db.onRampOrderRows)
+              ..where(
+                (tbl) => tbl.id.equals(orderId) & tbl.isCompleted.equals(false),
+              );
+
+            final isCompleted = data.status == OrderStatus.completed;
+
+            if (isCompleted) await _subscription?.cancel();
+
+            await statement.write(
+              OnRampOrderRowsCompanion(
+                humanStatus: Value(data.status.name),
+                machineStatus: Value(data.status.name),
+                isCompleted: Value(isCompleted),
+                // receiveAmount: Value(
+                //   ((data.payAmount.amount - data.totalFee.amount) /
+                //           data.quote.price *
+                //           pow(10, Currency.usdc.decimals))
+                //       .toInt(),
+                // ),
+              ),
+            );
+          }
+        });
+  }
+
+  void close() {
+    _subscription?.cancel();
+  }
+}
