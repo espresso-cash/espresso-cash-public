9fc35fd306e4e452725918cde8f84c644aa88b37
Justin Enerio <justinenerio@pm.me>
2023-11-29 15:37:23 +0800

    upd

--- packages/espressocash_backend/scalex_api/lib/src/constants.dart
+++ packages/espressocash_backend/scalex_api/lib/src/constants.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
 const scalexBaseUrl = 'https://ramp.scalex.africa';
-final scalexApiKey = Platform.environment['SCALEX_API_KEY'] ??
-    'test.sc.ey651c413fd36df7d23e338f53651c413fd36df7d23e338f54651c413fd36df7d23e338f55';
-final scalexSecretKey = Platform.environment['SCALEX_SECRET_KEY'] ??
-    'test_secret_651c413fd36df7d23e338f58651c413fd36df7d23e338f59';
+final scalexApiKey = Platform.environment['SCALEX_API_KEY'] ?? '';
+final scalexSecretKey = Platform.environment['SCALEX_SECRET_KEY'] ?? '';
