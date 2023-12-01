8b6880c9288eedf6e9b2b8191fff4fdc1d719df8
Justin Enerio <justinenerio@pm.me>
2023-12-01 12:37:43 +0800

    upd onboarding success

--- packages/espressocash_app/lib/features/onboarding/src/screens/success_screen.dart
+++ packages/espressocash_app/lib/features/onboarding/src/screens/success_screen.dart
@@ -4,10 +4,8 @@ import 'package:flutter/material.dart';
 import '../../../../../../gen/assets.gen.dart';
 import '../../../../../../l10n/l10n.dart';
 import '../../../../../../ui/button.dart';
-import '../../../../../../ui/content_padding.dart';
 import '../../../../../../ui/theme.dart';
 import '../../../../routes.gr.dart';
-import '../../../../ui/rounded_rectangle.dart';
 
 @RoutePage()
 class OnboardingSuccessScreen extends StatelessWidget {
@@ -20,35 +18,83 @@ class OnboardingSuccessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => CpTheme.black(
         child: Scaffold(
-          body: CpContentPadding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 110),
-                Assets.icons.successCheck.svg(width: 72, height: 72),
-                const SizedBox(height: 42),
-                CpRoundedRectangle(
-                  padding: const EdgeInsets.all(32),
-                  backgroundColor: Colors.black,
-                  child: Text(
-                    '${context.l10n.backupPhrase_lblSuccessMessage1}\n\n${context.l10n.backupPhrase_lblSuccessMessage2}',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w400,
-                    ),
+          body: LayoutBuilder(
+            builder: (context, viewportConstraints) => SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      const _Header(),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: CpButton(
+                          size: CpButtonSize.big,
+                          width: double.infinity,
+                          text: 'Finish',
+                          onPressed: onDone,
+                        ),
+                      ),
+                      const SizedBox(height: 48),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 80),
-                CpButton(
-                  size: CpButtonSize.big,
-                  width: double.infinity,
-                  text: context.l10n.ok,
-                  onPressed: onDone,
-                ),
-              ],
+              ),
             ),
           ),
+        ),
+      );
+}
+
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) => AspectRatio(
+        aspectRatio: 1,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Assets.images.sendManualBg.image(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Assets.icons.successCheck2.svg(height: 103, width: 103),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    context.l10n.backupPhrase_lblSuccessMessage1,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.23,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    context.l10n.backupPhrase_lblSuccessMessage2,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.19,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
 }
