44884dd158b6d373fdbfce173dc88c60d548c966
Justin Enerio <justinenerio@pm.me>
2024-01-17 00:47:16 +0800

    upd

--- packages/espressocash_app/lib/features/profile/service/wallet_service.dart
+++ packages/espressocash_app/lib/features/profile/service/wallet_service.dart
@@ -3,7 +3,6 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../accounts/models/ec_wallet.dart';
 import '../../authenticated/auth_scope.dart';
 import '../data/profile_repository.dart';
 
@@ -13,7 +12,6 @@ class WalletService implements Disposable {
     this._sharedPreferences,
     this._profileRepository,
     this._cryptopleaseClient,
-    this._wallet,
   ) {
     _profileRepository.onCountryUpdate = _handleCountryUpdate;
   }
@@ -21,7 +19,6 @@ class WalletService implements Disposable {
   final CryptopleaseClient _cryptopleaseClient;
   final SharedPreferences _sharedPreferences;
   final ProfileRepository _profileRepository;
-  final ECWallet _wallet;
 
   bool get _hasSyncedWalletCountry =>
       _sharedPreferences.getBool(_countrySyncedKey) ?? false;
@@ -37,13 +34,8 @@ class WalletService implements Disposable {
     final countryCode = _profileRepository.country;
 
     if (countryCode != null) {
-      final wallet = _wallet.address;
-
       _cryptopleaseClient.updateUserWalletCountry(
-        WalletCountryRequestDto(
-          walletAddress: wallet,
-          countryCode: countryCode,
-        ),
+        WalletCountryRequestDto(countryCode: countryCode),
       );
 
       _sharedPreferences.setBool(_countrySyncedKey, true);
