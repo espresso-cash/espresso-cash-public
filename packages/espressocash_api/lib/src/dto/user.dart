988d258583d5af71a6389c3fbe99e9d840941b9c
Justin Enerio <justinenerio@pm.me>
2024-01-16 23:05:34 +0800

    feat: add endpoint for storing wallet country

---
+++ packages/espressocash_api/lib/src/dto/user.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class WalletCountryRequestDto with _$WalletCountryRequestDto {
+  const factory WalletCountryRequestDto({
+    required String walletAddress,
+    required String countryCode,
+  }) = _WalletCountryRequestDto;
+
+  factory WalletCountryRequestDto.fromJson(Map<String, dynamic> json) =>
+      _$WalletCountryRequestDtoFromJson(json);
+}
