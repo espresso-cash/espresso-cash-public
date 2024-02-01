06952d90326c50a848e54385800b557316c48215
Justin Enerio <justinenerio@pm.me>
2024-02-01 19:35:34 +0800

    upd

--- packages/espressocash_app/lib/features/profile/widgets/learning_section.dart
+++ packages/espressocash_app/lib/features/profile/widgets/learning_section.dart
@@ -13,7 +13,7 @@ class LearningSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ProfileSection(
         title: context.l10n.learningSectionTitle,
-        padding: const EdgeInsets.symmetric(vertical: 16),
+        padding: const EdgeInsets.fromLTRB(8, 16, 2, 16),
         actions: [
           ProfileButton(
             label: context.l10n.contactUs,
