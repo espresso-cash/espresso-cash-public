f70ca6e1fd9674b49b175af0e5f852e74736f7f1
Justin Enerio <justinenerio@pm.me>
2024-03-01 15:05:14 +0800

    feat: add incoming dln api endpoint

--- packages/espressocash_api/lib/src/dto/dln_payment.dart
+++ packages/espressocash_api/lib/src/dto/dln_payment.dart
@@ -69,6 +69,57 @@ class OrderIdDlnResponseDto with _$OrderIdDlnResponseDto {
       _$OrderIdDlnResponseDtoFromJson(json);
 }
 
+@freezed
+class IncomingQuoteRequestDto with _$IncomingQuoteRequestDto {
+  const factory IncomingQuoteRequestDto({
+    required int amount,
+    required String senderAddress,
+    required String senderBlockchain,
+    required String receiverAddress,
+    required String? solanaReferenceAddress,
+  }) = _IncomingQuoteRequestDto;
+
+  factory IncomingQuoteRequestDto.fromJson(Map<String, dynamic> json) =>
+      _$IncomingQuoteRequestDtoFromJson(json);
+}
+
+@freezed
+class IncomingQuoteResponseDto with _$IncomingQuoteResponseDto {
+  const factory IncomingQuoteResponseDto({
+    required QuoteTx tx,
+    required QuoteUsdcInfo usdcInfo,
+    required int inputAmount,
+    required int receiverAmount,
+    required int feeInUsdc,
+  }) = _IncomingQuoteResponseDto;
+
+  factory IncomingQuoteResponseDto.fromJson(Map<String, dynamic> json) =>
+      _$IncomingQuoteResponseDtoFromJson(json);
+}
+
+@freezed
+class QuoteTx with _$QuoteTx {
+  const factory QuoteTx({
+    required String to,
+    required String data,
+    required int value,
+  }) = _QuoteTx;
+
+  factory QuoteTx.fromJson(Map<String, dynamic> json) =>
+      _$QuoteTxFromJson(json);
+}
+
+@freezed
+class QuoteUsdcInfo with _$QuoteUsdcInfo {
+  const factory QuoteUsdcInfo({
+    required String usdcAddress,
+    required int approvalAmount,
+  }) = _QuoteUsdcInfo;
+
+  factory QuoteUsdcInfo.fromJson(Map<String, dynamic> json) =>
+      _$QuoteUsdcInfoFromJson(json);
+}
+
 @JsonEnum(fieldRename: FieldRename.pascal)
 enum DlnOrderStatus {
   none,
