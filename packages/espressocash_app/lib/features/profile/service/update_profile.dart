7da86c01aa667b68e83be733a0516e342010813f
ookami-kb <ookami.kb@gmail.com>
2024-03-16 11:49:59 +0100

    feat: update profile error dialog

---
+++ packages/espressocash_app/lib/features/profile/service/update_profile.dart
@@ -0,0 +1,42 @@
+import 'package:dfunc/dfunc.dart';
+import 'package:espressocash_api/espressocash_api.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../core/errors.dart';
+import '../../intercom/services/intercom_service.dart';
+import '../data/profile_repository.dart';
+
+@injectable
+class UpdateProfile {
+  const UpdateProfile(
+    this._client,
+    this._intercomService,
+    this._profileRepository,
+  );
+
+  final EspressoCashClient _client;
+  final IntercomService _intercomService;
+  final ProfileRepository _profileRepository;
+
+  AsyncResult<void> call({
+    required String firstName,
+    required String lastName,
+    required String countryCode,
+    required String email,
+    required String? photoPath,
+  }) =>
+      tryEitherAsync((_) async {
+        if (_profileRepository.country != countryCode) {
+          final request = WalletCountryRequestDto(countryCode: countryCode);
+          await _client.updateUserWalletCountry(request);
+          _intercomService.updateCountry(countryCode);
+        }
+
+        _profileRepository
+          ..firstName = firstName
+          ..lastName = lastName
+          ..country = countryCode
+          ..photoPath = photoPath
+          ..email = email;
+      }).doOnLeftAsync(reportError);
+}
