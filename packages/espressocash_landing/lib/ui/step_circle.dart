d977a50cf57ce32f2a4cfad1de59bc00e27bdcfb
Justin Enerio <justinenerio@pm.me>
2024-03-24 00:05:15 +0800

    refactor: move landing code (#1335)
    Co-authored-by: ookami-kb <ookami.kb@gmail.com>

---
+++ packages/espressocash_landing/lib/ui/step_circle.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../utils/extensions.dart';
+
+class StepCircle extends StatelessWidget {
+  const StepCircle(this.number, {super.key});
+
+  final int number;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        width: isMobile ? 36 : 46,
+        height: isMobile ? 36 : 46,
+        decoration: const ShapeDecoration(
+          color: Colors.black,
+          shape: OvalBorder(),
+        ),
+        child: Center(
+          child: Text(
+            '$number',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      );
+}
