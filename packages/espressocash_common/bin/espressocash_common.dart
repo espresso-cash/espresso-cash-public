fc1c1980131a7a9f21195cdad97b4315154902ff
Justin Enerio <justinenerio@pm.me>
2024-03-27 11:53:47 +0800

    added initial files

---
+++ packages/espressocash_common/bin/espressocash_common.dart
@@ -0,0 +1,5 @@
+import 'package:espressocash_common/espressocash_common.dart' as espressocash_common;
+
+void main(List<String> arguments) {
+  print('Hello world: ${espressocash_common.calculate()}!');
+}
