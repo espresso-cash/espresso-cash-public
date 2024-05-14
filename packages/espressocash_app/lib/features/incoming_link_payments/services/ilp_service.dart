92554cb6243f1c06c0e0941601d42631557e7c42
Justin Enerio <justinenerio@pm.me>
2024-05-14 19:12:16 +0700

    refactor watchers

--- packages/espressocash_app/lib/features/incoming_link_payments/services/ilp_service.dart
+++ packages/espressocash_app/lib/features/incoming_link_payments/services/ilp_service.dart
@@ -1,12 +1,21 @@
+import 'dart:async';
+
 import 'package:dfunc/dfunc.dart';
 import 'package:dio/dio.dart';
 import 'package:espressocash_api/espressocash_api.dart';
+import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:solana/encoder.dart';
 import 'package:solana/solana.dart';
 import 'package:uuid/uuid.dart';
 
 import '../../../utils/errors.dart';
+import '../../accounts/auth_scope.dart';
 import '../../accounts/models/ec_wallet.dart';
+import '../../balances/services/refresh_balance.dart';
+import '../../currency/models/amount.dart';
+import '../../currency/models/currency.dart';
 import '../../escrow/models/escrow_private_key.dart';
 import '../../escrow_payments/create_incoming_escrow.dart';
 import '../../transactions/models/tx_results.dart';
@@ -14,12 +23,40 @@ import '../../transactions/services/resign_tx.dart';
 import '../data/ilp_repository.dart';
 import '../models/incoming_link_payment.dart';
 
-@injectable
-class ILPService {
-  const ILPService(this._repository, this._createIncomingEscrow);
+@Singleton(scope: authScope)
+class ILPService implements Disposable {
+  ILPService(
+    this._repository,
+    this._createIncomingEscrow,
+    this._ecClient,
+    this._refreshBalance,
+  );
 
   final ILPRepository _repository;
   final CreateIncomingEscrow _createIncomingEscrow;
+  final EspressoCashClient _ecClient;
+  final RefreshBalance _refreshBalance;
+
+  final Map<String, StreamSubscription<void>> _subscriptions = {};
+
+  void _subscribe(String id) {
+    _subscriptions[id] = _repository
+        .watch(id)
+        .whereNotNull()
+        .asyncExpand<IncomingLinkPayment?>((payment) {
+      switch (payment.status) {
+        case ILPStatusTxCreated():
+          return _send(payment).asStream();
+        case ILPStatusTxSent():
+          return _wait(payment).asStream();
+        case ILPStatusSuccess():
+        case ILPStatusTxFailure():
+          _subscriptions.remove(id)?.cancel();
+
+          return null;
+      }
+    }).listen((payment) => payment?.let(_repository.save));
+  }
 
   Future<IncomingLinkPayment> create({
     required ECWallet account,
@@ -40,6 +77,7 @@ class ILPService {
     );
 
     await _repository.save(payment);
+    _subscribe(id);
 
     return payment;
   }
@@ -56,6 +94,7 @@ class ILPService {
     final newPayment = payment.copyWith(status: status);
 
     await _repository.save(newPayment);
+    _subscribe(payment.id);
 
     return newPayment;
   }
@@ -93,4 +132,72 @@ class ILPService {
       );
     }
   }
+
+  Future<IncomingLinkPayment?> _send(IncomingLinkPayment payment) async {
+    final status = payment.status;
+    if (status is! ILPStatusTxCreated) {
+      return payment;
+    }
+
+    final tx = status.tx;
+
+    try {
+      await _ecClient.submitDurableTx(
+        SubmitDurableTxRequestDto(
+          tx: tx.encode(),
+        ),
+      );
+
+      return payment.copyWith(
+        status: ILPStatus.txSent(tx),
+      );
+    } on Exception catch (ex) {
+      //TODO update error handling
+      return payment.copyWith(
+        status: const ILPStatus.txFailure(
+          reason: TxFailureReason.creatingFailure,
+        ),
+      );
+    }
+  }
+
+  Future<IncomingLinkPayment?> _wait(IncomingLinkPayment payment) async {
+    final status = payment.status;
+
+    if (status is! ILPStatusTxSent) {
+      return payment;
+    }
+
+    int? fee;
+    try {
+      fee = status.tx.containsAta
+          ? await _ecClient.getFees().then((value) => value.escrowPaymentAtaFee)
+          : null;
+    } on Object {
+      fee = null;
+    }
+
+    _refreshBalance();
+
+    return payment.copyWith(
+      status: ILPStatus.success(
+        tx: status.tx,
+        fee: fee?.let(
+          (fee) => CryptoAmount(value: fee, cryptoCurrency: Currency.usdc),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Future<void> onDispose() async {
+    await Future.wait(_subscriptions.values.map((it) => it.cancel()));
+  }
+}
+
+extension on SignedTx {
+  bool get containsAta => decompileMessage().let(
+        (m) => m.instructions
+            .any((ix) => ix.programId == AssociatedTokenAccountProgram.id),
+      );
 }
--- packages/espressocash_app/lib/features/outgoing_link_payments/services/olp_service.dart
+++ packages/espressocash_app/lib/features/outgoing_link_payments/services/olp_service.dart
@@ -1,30 +1,73 @@
+import 'dart:async';
+
 import 'package:dfunc/dfunc.dart';
+import 'package:espressocash_api/espressocash_api.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:get_it/get_it.dart';
 
 import 'package:injectable/injectable.dart';
+import 'package:solana/base58.dart';
 import 'package:solana/solana.dart';
 import 'package:uuid/uuid.dart';
 
+import '../../accounts/auth_scope.dart';
 import '../../accounts/models/ec_wallet.dart';
 import '../../currency/models/amount.dart';
 import '../../escrow/models/escrow_private_key.dart';
 import '../../escrow_payments/create_canceled_escrow.dart';
 import '../../escrow_payments/create_outgoing_escrow.dart';
+import '../../link_payments/models/link_payment.dart';
 import '../../transactions/models/tx_results.dart';
 import '../../transactions/services/resign_tx.dart';
 import '../data/repository.dart';
 import '../models/outgoing_link_payment.dart';
 
-@injectable
-class OLPService {
-  const OLPService(
+@Singleton(scope: authScope)
+class OLPService implements Disposable {
+  OLPService(
     this._repository,
     this._createOutgoingEscrow,
     this._createCanceledEscrow,
+    this._ecClient,
   );
 
   final OLPRepository _repository;
   final CreateOutgoingEscrow _createOutgoingEscrow;
   final CreateCanceledEscrow _createCanceledEscrow;
+  final EspressoCashClient _ecClient;
+
+  final Map<String, StreamSubscription<void>> _subscriptions = {};
+
+  void _watchReady() {}
+
+  void _subscribe(String id) {
+    _subscriptions[id] =
+        _repository.watch(id).asyncExpand<OutgoingLinkPayment>((payment) {
+      switch (payment.status) {
+        case OLPStatusTxCreated():
+          return _sendOutgoing(payment).asStream();
+        case OLPStatusTxSent():
+          return Stream.value(_wait(payment));
+
+        case OLPStatusLinkReady():
+        case OLPStatusWithdrawn():
+        case OLPStatusCanceled():
+          _subscriptions.remove(id)?.cancel();
+
+          return null;
+
+        case OLPStatusCancelTxCreated():
+          return _sendCanceled(payment).asStream();
+
+        case OLPStatusCancelTxSent():
+          return Stream.value(_processCanceled(payment));
+
+        case OLPStatusTxFailure():
+        case OLPStatusCancelTxFailure():
+          return null;
+      }
+    }).listen((payment) => payment.let(_repository.save));
+  }
 
   Future<OutgoingLinkPayment> create({
     required CryptoAmount amount,
@@ -45,6 +88,7 @@ class OLPService {
     );
 
     await _repository.save(payment);
+    _subscribe(id);
 
     return payment;
   }
@@ -61,6 +105,7 @@ class OLPService {
     final newPayment = payment.copyWith(status: status);
 
     await _repository.save(newPayment);
+    _subscribe(payment.id);
 
     return newPayment;
   }
@@ -126,6 +171,7 @@ class OLPService {
         escrow: privateKey,
       );
     } on Exception {
+      //TODO update error handling
       return const OLPStatus.txFailure(
         reason: TxFailureReason.creatingFailure,
       );
@@ -156,5 +202,116 @@ class OLPService {
         reason: TxFailureReason.creatingFailure,
       );
     }
+  }
+
+  Future<OutgoingLinkPayment> _sendOutgoing(OutgoingLinkPayment payment) async {
+    final status = payment.status;
+    if (status is! OLPStatusTxCreated) {
+      return payment;
+    }
+
+    final tx = status.tx;
+
+    try {
+      await _ecClient.submitDurableTx(
+        SubmitDurableTxRequestDto(
+          tx: tx.encode(),
+        ),
+      );
+
+      return payment.copyWith(
+        status: OLPStatus.txSent(
+          tx,
+          escrow: status.escrow,
+        ),
+      );
+    } on Exception catch (ex) {
+      //TODO update error handling
+      print(ex);
+
+      return payment.copyWith(
+        status: const OLPStatus.txFailure(
+          reason: TxFailureReason.creatingFailure, //TODO
+        ),
+      );
+    }
+  }
+
+  OutgoingLinkPayment _wait(OutgoingLinkPayment payment) {
+    //TODO rename fn
+    final status = payment.status;
+    if (status is! OLPStatusTxSent) {
+      return payment;
+    }
+
+    final token = payment.amount.token;
+
+    final privateKey = status.escrow.bytes.lock;
+    final key = base58encode(privateKey.toList());
+
+    final link = LinkPayment(
+      key: key,
+      token: token.publicKey,
+    ).toShareableLink();
+
+    final newStatus = OLPStatus.linkReady(
+      link: link,
+      escrow: status.escrow,
+    );
+
+    return payment.copyWith(status: newStatus);
+  }
+
+  Future<OutgoingLinkPayment> _sendCanceled(
+    OutgoingLinkPayment payment,
+  ) async {
+    final status = payment.status;
+    if (status is! OLPStatusCancelTxCreated) {
+      return payment;
+    }
+
+    final tx = status.tx;
+
+    try {
+      await _ecClient.submitDurableTx(
+        SubmitDurableTxRequestDto(
+          tx: tx.encode(),
+        ),
+      );
+
+      return payment.copyWith(
+        status: OLPStatus.cancelTxSent(
+          tx,
+          escrow: status.escrow,
+        ),
+      );
+    } on Exception catch (ex) {
+      print(ex);
+
+      return payment.copyWith(
+        status: OLPStatus.cancelTxFailure(
+          reason: TxFailureReason.creatingFailure, //TODO
+          escrow: status.escrow,
+        ),
+      );
+    }
+  }
+
+  OutgoingLinkPayment _processCanceled(OutgoingLinkPayment payment) {
+    final status = payment.status;
+
+    return status is! OLPStatusCancelTxSent
+        ? payment
+        : payment.copyWith(
+            status: OLPStatus.canceled(
+              txId: status.tx.id,
+              timestamp: DateTime.now(),
+            ),
+          );
+  }
+
+  @override
+  Future<void> onDispose() async {
+    await Future.wait(_subscriptions.values.map((it) => it.cancel()));
   }
 }
