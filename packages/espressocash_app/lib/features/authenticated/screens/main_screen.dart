b2677313af650cd5e993a376fb9162a80f36d107
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-06-04 13:11:13 +0300

    wip: add divider color do CpColors and add Divider to HomeScreen

--- packages/espressocash_app/lib/features/authenticated/screens/main_screen.dart
+++ packages/espressocash_app/lib/features/authenticated/screens/main_screen.dart
@@ -92,15 +92,7 @@ class _MainContentState extends State<_MainContent> {
   Widget build(BuildContext context) => Container(
         padding: const EdgeInsets.only(bottom: cpNavigationBarheight),
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              CpColors.darkGoldBackgroundColor,
-              CpColors.dashboardBackgroundColor,
-            ],
-            stops: [0.49, 0.51],
-          ),
+          color: CpColors.dashboardBackgroundColor,
         ),
         child: RefreshBalancesWrapper(
           builder: (context, onRefresh) => RefreshIndicator(
@@ -137,6 +129,15 @@ class _MainContentState extends State<_MainContent> {
                       color: CpColors.homeDividerColor,
                       thickness: 1.0,
                       height: 1.0,
+                    ),
+                  ),
+                ),
+                const SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 24),
+                    child: Divider(
+                      color: CpColors.homeDividerColor,
+                      thickness: 1.0,
                     ),
                   ),
                 ),
