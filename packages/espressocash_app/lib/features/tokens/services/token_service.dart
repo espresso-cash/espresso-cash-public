f686021046b23cd8ee42367844810004af5a29c0
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-06-14 16:08:59 -0300

    wip: add isolate to init token db row

--- packages/espressocash_app/lib/features/tokens/services/token_service.dart
+++ packages/espressocash_app/lib/features/tokens/services/token_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:collection/collection.dart';
 import 'package:dfunc/dfunc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 
@@ -18,35 +19,35 @@ class TokenService {
   Future<Either<Exception, void>> initializeDatabaseFromCsvFile(
     String filePath,
   ) =>
-      tryEitherAsync(
-        (_) async {
-          String? data = await rootBundle.loadString(filePath);
+      tryEitherAsync((_) async {
+        final String data = await rootBundle.loadString(filePath);
+        await compute(_parseAndInsertTokens, data);
+      });
 
-          await tokenRepository.clearAllTokens();
+  Future<void> _parseAndInsertTokens(String data) async {
+    final lines = const LineSplitter().convert(data).skip(1);
 
-          final lines = const LineSplitter().convert(data).skip(1);
+    await tokenRepository.clearAllTokens();
 
-          for (final line in lines) {
-            final values = line.split(',');
-            final tags = _parseTags(values[6]);
-            final extensions = _parseExtensions(values[7]);
+    for (final line in lines) {
+      final values = line.split(',');
+      final tags = _parseTags(values[6]);
+      final extensions = _parseExtensions(values[7]);
 
-            final tokenRow = TokenRow(
-              chainId: int.parse(values[1]),
-              address: values[0],
-              symbol: values[2],
-              name: values[3],
-              decimals: int.parse(values[4]),
-              logoURI: values[5],
-              tags: tags,
-              extensions: extensions,
-            );
-            await tokenRepository.insertToken(tokenRow);
-          }
-
-          data = null;
-        },
+      final tokenRow = TokenRow(
+        chainId: int.parse(values[1]),
+        address: values[0],
+        symbol: values[2],
+        name: values[3],
+        decimals: int.parse(values[4]),
+        logoURI: values[5],
+        tags: tags,
+        extensions: extensions,
       );
+
+      await tokenRepository.insertToken(tokenRow);
+    }
+  }
 
   List<String>? _parseTags(String? tagString) {
     if (tagString == null || tagString.isEmpty) return null;
