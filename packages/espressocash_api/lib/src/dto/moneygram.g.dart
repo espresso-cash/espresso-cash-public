e1a276ab2e89e55586e1362f788720b6ee4e79f3
Justin Enerio <justinenerio@pm.me>
2024-06-26 22:41:50 +0800

    upd api

--- packages/espressocash_api/lib/src/dto/moneygram.g.dart
+++ packages/espressocash_api/lib/src/dto/moneygram.g.dart
@@ -6,40 +6,6 @@ part of 'moneygram.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$MoneygramDepositRequestDtoImpl _$$MoneygramDepositRequestDtoImplFromJson(
-        Map<String, dynamic> json) =>
-    _$MoneygramDepositRequestDtoImpl(
-      signedTx: json['signedTx'] as String,
-      account: json['account'] as String,
-      amount: json['amount'] as String,
-      lang: json['lang'] as String,
-    );
-
-Map<String, dynamic> _$$MoneygramDepositRequestDtoImplToJson(
-        _$MoneygramDepositRequestDtoImpl instance) =>
-    <String, dynamic>{
-      'signedTx': instance.signedTx,
-      'account': instance.account,
-      'amount': instance.amount,
-      'lang': instance.lang,
-    };
-
-_$MoneygramDepositResponseDtoImpl _$$MoneygramDepositResponseDtoImplFromJson(
-        Map<String, dynamic> json) =>
-    _$MoneygramDepositResponseDtoImpl(
-      id: json['id'] as String,
-      url: json['url'] as String,
-      token: json['token'] as String,
-    );
-
-Map<String, dynamic> _$$MoneygramDepositResponseDtoImplToJson(
-        _$MoneygramDepositResponseDtoImpl instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'url': instance.url,
-      'token': instance.token,
-    };
-
 _$MoneygramChallengeSignRequestDtoImpl
     _$$MoneygramChallengeSignRequestDtoImplFromJson(
             Map<String, dynamic> json) =>
