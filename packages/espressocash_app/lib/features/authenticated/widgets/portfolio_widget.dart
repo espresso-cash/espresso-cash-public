6b437ee83e83ed882148725922e11b061914b59d
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-07-08 11:38:17 -0300

    fix: merge imports

--- packages/espressocash_app/lib/features/authenticated/widgets/portfolio_widget.dart
+++ packages/espressocash_app/lib/features/authenticated/widgets/portfolio_widget.dart
@@ -11,7 +11,6 @@ import '../../../ui/value_stream_builder.dart';
 import '../../conversion_rates/services/token_fiat_balance_service.dart';
 import '../../conversion_rates/widgets/extensions.dart';
 import '../../currency/models/amount.dart';
-import '../../currency/models/currency.dart';
 import '../../token_details/screens/token_details_screen.dart';
 import '../../tokens/widgets/token_icon.dart';
 
