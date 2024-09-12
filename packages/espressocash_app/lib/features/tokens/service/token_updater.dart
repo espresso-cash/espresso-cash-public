6be502c4f2d56ead3df879f22dd84c6df40601c1
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-09-12 19:49:49 -0300

    feat: store token list localdb (#1498)
    Co-authored-by: Kirill Bubochkin <ookami.kb@gmail.com>
    Co-authored-by: Justin Enerio <justinenerio@pm.me>
    Co-authored-by: Vlad Sumin <vladsum00@gmail.com>

---
+++ packages/espressocash_app/lib/features/tokens/service/token_updater.dart
@@ -0,0 +1,41 @@
+import 'dart:core';
+
+import 'package:dfunc/dfunc.dart';
+import 'package:espressocash_api/espressocash_api.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../data/file_manager.dart';
+import '../../accounts/auth_scope.dart';
+import '../data/token_repository.dart';
+
+@Singleton(scope: authScope)
+class TokenUpdater {
+  const TokenUpdater(
+    this.fileManager,
+    this._ecClient,
+    this._tokenRepository,
+    this._tokensMetaStorage,
+  );
+
+  final FileManager fileManager;
+  final EspressoCashClient _ecClient;
+  final TokenRepository _tokenRepository;
+  final TokensMetaStorage _tokensMetaStorage;
+
+  @PostConstruct(preResolve: false)
+  Future<void> call() => tryEitherAsync((_) async {
+        final actualHash = _tokensMetaStorage.getHash();
+        final rootToken = ServicesBinding.rootIsolateToken;
+
+        if (rootToken == null) return;
+
+        final serverHash = await _ecClient.getTokensMeta();
+        // ignore: avoid-weak-cryptographic-algorithms, non sensitive
+        final shouldInitialize = serverHash.md5 != actualHash;
+
+        if (shouldInitialize) {
+          await _tokenRepository.update(_ecClient);
+        }
+      });
+}
