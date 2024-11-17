c26e06c9f6641942182fb19d32db5ce9fb637f7e
Justin Enerio <justinenerio@pm.me>
2024-11-18 00:07:54 +1100

    initial api upd

---
+++ packages/espressocash_api/lib/src/dto/ambassador_referral.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ambassador_referral.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AmbassadorReferralRequestDtoImpl _$$AmbassadorReferralRequestDtoImplFromJson(
+        Map<String, dynamic> json) =>
+    _$AmbassadorReferralRequestDtoImpl(
+      ambassadorAddress: json['ambassadorAddress'] as String,
+      userAddress: json['userAddress'] as String,
+    );
+
+Map<String, dynamic> _$$AmbassadorReferralRequestDtoImplToJson(
+        _$AmbassadorReferralRequestDtoImpl instance) =>
+    <String, dynamic>{
+      'ambassadorAddress': instance.ambassadorAddress,
+      'userAddress': instance.userAddress,
+    };
