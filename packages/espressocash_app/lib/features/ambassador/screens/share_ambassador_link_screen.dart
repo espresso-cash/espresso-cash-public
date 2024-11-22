a0916d24d9cfa9f82e27f7c7b960f45f66295b98
Justin Enerio <justinenerio@pm.me>
2024-11-22 22:13:05 +0800

    fix dep cycle

--- packages/espressocash_app/lib/features/ambassador/screens/share_ambassador_link_screen.dart
+++ packages/espressocash_app/lib/features/ambassador/screens/share_ambassador_link_screen.dart
@@ -1,61 +1,59 @@
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:solana/solana.dart';
 
-import '../../../di.dart';
 import '../../../ui/colors.dart';
-import '../../accounts/models/ec_wallet.dart';
-import '../../profile/data/profile_repository.dart';
 import '../models/ambassador_referral.dart';
 import '../widgets/ambassador_page.dart';
 
-class ShareAmbassadorLinkScreen extends StatefulWidget {
-  const ShareAmbassadorLinkScreen({super.key});
+class ShareAmbassadorLinkScreen extends StatelessWidget {
+  const ShareAmbassadorLinkScreen({
+    super.key,
+    required this.name,
+    required this.address,
+  });
 
-  static void push(BuildContext context) => Navigator.of(context).push<void>(
+  final String name;
+  final Ed25519HDPublicKey address;
+
+  static void push(
+    BuildContext context, {
+    required String name,
+    required Ed25519HDPublicKey address,
+  }) =>
+      Navigator.of(context).push<void>(
         MaterialPageRoute(
-          builder: (context) => const ShareAmbassadorLinkScreen(),
-        ),
-      );
-
-  @override
-  State<ShareAmbassadorLinkScreen> createState() =>
-      _ShareAmbassadorLinkScreenState();
-}
-
-class _ShareAmbassadorLinkScreenState extends State<ShareAmbassadorLinkScreen> {
-  late final Uri _uri;
-  late final String _name;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final wallet = sl<ECWallet>().publicKey;
-    final referral = AmbassadorReferral(address: wallet);
-
-    _uri = referral.toLink();
-    _name = sl<ProfileRepository>().fullName;
-  }
-
-  @override
-  Widget build(BuildContext context) => AmbassadorPage(
-        name: _name,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
-          child: Column(
-            children: [
-              const SizedBox(height: 8),
-              Center(
-                child: BarcodeWidget(
-                  height: 220,
-                  barcode: Barcode.qrCode(),
-                  data: _uri.toString(),
-                  padding: EdgeInsets.zero,
-                  color: CpColors.mediumSandColor,
-                ),
-              ),
-            ],
+          builder: (context) => ShareAmbassadorLinkScreen(
+            name: name,
+            address: address,
           ),
         ),
       );
+
+  @override
+  Widget build(BuildContext context) {
+    final referral = AmbassadorReferral(address: address);
+    final uri = referral.toLink();
+
+    return AmbassadorPage(
+      name: name,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Center(
+              child: BarcodeWidget(
+                height: 220,
+                barcode: Barcode.qrCode(),
+                data: uri.toString(),
+                padding: EdgeInsets.zero,
+                color: CpColors.mediumSandColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- packages/espressocash_app/lib/features/profile/widgets/ambassador_section.dart
+++ packages/espressocash_app/lib/features/profile/widgets/ambassador_section.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../../../di.dart';
 import '../../../l10n/gen/app_localizations.dart';
+import '../../accounts/models/ec_wallet.dart';
 import '../../ambassador/screens/share_ambassador_link_screen.dart';
 import '../../feature_flags/services/feature_flags_manager.dart';
+import '../data/profile_repository.dart';
 import 'profile_button.dart';
 import 'profile_section.dart';
 
@@ -23,7 +25,16 @@ class AmbassadorSection extends StatelessWidget {
               ProfileButton(
                 label: l10n.ambassador_userOnboardingLabel,
                 description: l10n.ambassador_userOnboardingDescription,
-                onPressed: () => ShareAmbassadorLinkScreen.push(context),
+                onPressed: () {
+                  final wallet = sl<ECWallet>().publicKey;
+                  final name = sl<ProfileRepository>().fullName;
+
+                  ShareAmbassadorLinkScreen.push(
+                    context,
+                    name: name,
+                    address: wallet,
+                  );
+                },
               ),
             ],
           );
