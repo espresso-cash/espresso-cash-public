17d100ad5e019d4499db9b2ca3bb0e90b14158eb
Vlad Sumin <vladsum00@gmail.com>
2024-12-13 20:37:32 +0200

    upd

--- packages/espressocash_app/lib/features/activities/widgets/no_activity.dart
+++ packages/espressocash_app/lib/features/activities/widgets/no_activity.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../gen/assets.gen.dart';
 import '../../../../l10n/l10n.dart';
 import '../../../../ui/navigation_bar/navigation_bar.dart';
-import '../../../ui/markdown_text.dart';
 
 class NoActivity extends StatelessWidget {
   const NoActivity({super.key});
@@ -17,8 +16,16 @@ class NoActivity extends StatelessWidget {
           SizedBox(height: 30.h),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 44),
-            child: EcMarkdownText(
-              text: context.l10n.noActivityTitle.toUpperCase(),
+            child: Text(
+              context.l10n.noActivityTitle.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontStyle: FontStyle.normal,
+                fontSize: 32.sp,
+                fontWeight: FontWeight.w900,
+                letterSpacing: 0.25,
+                height: 1,
+              ),
             ),
           ),
           SizedBox(height: 12.h),
@@ -26,9 +33,11 @@ class NoActivity extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 44),
             child: Text(
               context.l10n.noActivityDescription,
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16.sp,
                 fontWeight: FontWeight.w400,
+                letterSpacing: 0.19,
                 height: 1.3,
               ),
             ),
