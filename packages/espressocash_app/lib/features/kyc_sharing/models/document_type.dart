ffcd19efa48b8d2c1e24495f03ef837956e691d9
Vlad Sumin <vladsum00@gmail.com>
2024-12-16 01:06:58 +0200

    upd document type

--- packages/espressocash_app/lib/features/kyc_sharing/models/document_type.dart
+++ packages/espressocash_app/lib/features/kyc_sharing/models/document_type.dart
@@ -1,11 +1,6 @@
 import 'package:kyc_client_dart/kyc_client_dart.dart';
 
 enum DocumentType {
-  bvn('BVN', 'BVN'),
-  nin('NIN', 'NIN'),
-  ninV2('NIN V2', 'NIN_V2'),
-  ninSlip('NIN SLIP', 'NIN_SLIP'),
-  driversLicense('Driver\'s License', 'DRIVERS_LICENSE'),
   voterId('Voter ID', 'VOTER_ID');
 
   const DocumentType(this.name, this.value);
@@ -16,19 +11,12 @@ enum DocumentType {
 
 extension DocumentTypeExtension on DocumentType {
   IdType toIdType() => switch (this) {
-        DocumentType.bvn ||
-        DocumentType.nin ||
-        DocumentType.ninV2 ||
-        DocumentType.ninSlip =>
-          IdType.other,
-        DocumentType.driversLicense => IdType.driverLicense,
         DocumentType.voterId => IdType.voterId,
       };
 }
 
 extension IdTypeExtension on IdType {
   DocumentType? toDocumentType() => switch (this) {
-        IdType.driverLicense => DocumentType.driversLicense,
         IdType.voterId => DocumentType.voterId,
         // ignore: avoid-wildcard-cases-with-enums, check if needed
         _ => null,
