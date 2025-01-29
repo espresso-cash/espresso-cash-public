14b90072d0d649bdddd904a578f978f79d580395
Justin Enerio <justinenerio@pm.me>
2025-01-29 23:39:07 +0800

    fix analyzer

--- packages/espressocash_app/lib/features/ramp/partners/brij/widgets/launch.dart
+++ packages/espressocash_app/lib/features/ramp/partners/brij/widgets/launch.dart
@@ -1,6 +1,5 @@
 import 'package:decimal/decimal.dart';
 import 'package:dfunc/dfunc.dart';
-import 'package:espressocash_api/espressocash_api.dart';
 import 'package:flutter/material.dart';
 import 'package:kyc_client_dart/kyc_client_dart.dart';
 
