c2a272580eab41ca3b953fe2c36f4a4ed9b395ed
Vlad Sumin <vladsum00@gmail.com>
2025-05-19 13:25:28 +0300

    upd

--- packages/espressocash_app/lib/features/profile/data/profile_repository.dart
+++ packages/espressocash_app/lib/features/profile/data/profile_repository.dart
@@ -1,4 +1,3 @@
-import 'package:dfunc/dfunc.dart';
 import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
